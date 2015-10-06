763e8549e568d12604039edc96ffe5286949dda0
Christian Lipphardt <christian.lipphardt@googlemail.com>
2015-10-06 11:42:52 +0200

    chore(distro): build osx/windows/linux distribution packages
    closes #6

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,20 @@
 
 var gulp = require('gulp'),
     run = require('gulp-run'),
-    electron = require('electron-connect').server.create();
+    packager = require('electron-packager'),
+    electron = require('electron-connect').server.create(),
+    _ = require('lodash');
 
+var ELECTRON_PACKAGER_VERSION='0.33.0'
+var PACKAGING_OPTS = {
+  name: 'camunda-modeler',
+  version: ELECTRON_PACKAGER_VERSION,
+  dir: '.',
+  out: 'distro',
+  //platform: 'all', # will be set in dist-<platform> task
+  arch: 'all',
+  overwrite: true
+}
 
 gulp.task('serve', function () {
 
@@ -24,5 +36,40 @@ gulp.task('run', function() {
 gulp.task('debug', function() {
   return run('electron --debug-brk=5858 .').exec();
 });
+
+gulp.task('dist-windows', function() {
+  var opts = {
+    platform: 'win32'
+  }
+  opts = _.merge(PACKAGING_OPTS, opts)
+
+  return packager(opts, function(err, appPath) {
+    return appPath;
+  });
+});
+
+gulp.task('dist-osx', function() {
+  var opts = {
+    platform: 'darwin'
+  }
+  opts = _.merge(PACKAGING_OPTS, opts)
+
+  return packager(opts, function(err, appPath) {
+    return appPath;
+  });
+});
+
+gulp.task('dist-linux', function() {
+  var opts = {
+    platform: 'linux'
+  }
+  opts = _.merge(PACKAGING_OPTS, opts)
+
+  return packager(opts, function(err, appPath) {
+    return appPath;
+  });
+});
+
+gulp.task('dist', ['dist-windows', 'dist-darwin', 'dist-linux']);
 
 gulp.task('default', [ 'serve' ]);
