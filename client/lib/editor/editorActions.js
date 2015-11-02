308d19a09aeb76301ee62891dc5ff9cc929ffaaf
pedesen <patrick.dehn@camunda.com>
2015-11-02 15:58:46 +0100

    fix(editor): ignore keyboard shortcuts when editing text
    Closes #39

--- client/lib/editor/editorActions.js
+++ client/lib/editor/editorActions.js
@@ -5,67 +5,124 @@ var browser = require('../util/browser');
 // TODO: Unregistering event
 
 function EditorActions(editor) {
+  var self = this;
+
+  this.editor = editor;
 
   browser.on('editor.actions', function(payload) {
-    switch (payload.event) {
-      // File
-      case 'file.open':
-        editor.openDiagram();
-        break;
-      case 'file.save':
-        editor.save(payload.data.create);
-        break;
-      case 'file.add':
-        editor.addDiagram(payload.data.diagram);
-        break;
+    var actions = self.getActions(payload),
+        evt = payload.event,
+        action = actions[evt];
 
-      case 'editor.new':
-        editor.newDiagram();
-        break;
-      case 'editor.close':
-        editor.closeDiagram(editor.currentDiagram);
-        break;
-      case 'editor.quit':
-        editor.quit();
-        break;
-
-      // Edit
-      case 'editor.spaceTool':
-        editor.trigger('spaceTool');
-        break;
-      case 'editor.lassoTool':
-        editor.trigger('lassoTool');
-        break;
-      case 'editor.directEditing':
-        editor.trigger('directEditing');
-        break;
-      case 'editor.moveCanvas':
-        editor.trigger('moveCanvas', payload.data);
-        break;
-      case 'editor.selectElements':
-        editor.trigger('selectElements', payload.data);
-        break;
-      case 'editor.removeSelection':
-        editor.trigger('removeSelection', payload.data);
-        break;
-      case 'editor.undo':
-        editor.trigger('undo');
-        break;
-      case 'editor.redo':
-        editor.trigger('redo');
-        break;
-
-      // Window
-      case 'editor.stepZoom':
-        editor.trigger('stepZoom', payload.data);
-        break;
-      case 'editor.zoom':
-        editor.trigger('zoom', payload.data);
-        break;
-      default:
-      return;
+    // Call the corresponding action function. If an input is active in the document
+    // (e.g. the user is currently writing in a textarea) and the ignoreIfActiveInput
+    // flag is set, ignore the action.
+    if (action && !(action.ignoreIfActiveInput && self.isInputActive())) {
+      action.handler();
     }
   });
 }
+
+EditorActions.prototype.getActions = function (payload) {
+  var editor = this.editor;
+
+  var actions = {
+    'file.open': {
+      handler: function() {
+        editor.openDiagram();
+      }
+    },
+    'file.save': {
+      handler: function() {
+        editor.save(payload.data.create);
+      }
+    },
+    'file.add': {
+      handler: function() {
+        editor.addDiagram(payload.data.diagram);
+      }
+    },
+    'editor.new': {
+      handler: function() {
+        editor.newDiagram();
+      }
+    },
+    'editor.close': {
+      handler: function() {
+        editor.closeDiagram(editor.currentDiagram);
+      }
+    },
+    'editor.quit': {
+      handler: function() {
+        editor.quit();
+      }
+    },
+    'editor.spaceTool': {
+      handler: function() {
+        editor.trigger('spaceTool');
+      },
+      ignoreIfActiveInput: true
+    },
+    'editor.lassoTool': {
+      handler: function() {
+        editor.trigger('lassoTool');
+      },
+      ignoreIfActiveInput: true
+    },
+    'editor.directEditing': {
+      handler: function() {
+        editor.trigger('directEditing');
+      },
+      ignoreIfActiveInput: true
+    },
+    'editor.moveCanvas': {
+      handler: function() {
+        editor.trigger('moveCanvas', payload.data);
+      },
+      ignoreIfActiveInput: true
+    },
+    'editor.selectElements': {
+      handler: function() {
+        editor.trigger('selectElements', payload.data);
+      }
+    },
+    'editor.removeSelection': {
+      handler: function() {
+        editor.trigger('removeSelection', payload.data);
+      }
+    },
+    'editor.undo': {
+      handler: function() {
+        editor.trigger('undo');
+      }
+    },
+    'editor.redo': {
+      handler: function() {
+        editor.trigger('redo');
+      }
+    },
+    'editor.stepZoom': {
+      handler: function() {
+        editor.trigger('stepZoom', payload.data);
+      }
+    },
+    'editor.zoom': {
+      handler: function() {
+        editor.trigger('zoom', payload.data);
+      }
+    }
+  };
+
+  return actions;
+};
+
+/**
+ * Returns true, if an input or textarea element is currently active
+ */
+EditorActions.prototype.isInputActive = function() {
+  var tagName = document.activeElement.tagName;
+
+  return tagName === 'TEXTAREA' || tagName === 'INPUT';
+};
 
 module.exports = EditorActions;
