ecd9bb97ad3aa2de0bc156c65a01885ddbe5981b
Ricardo Matias <ricardo.matias0@gmail.com>
2015-11-23 15:47:51 +0100

    feat(menu-bar): add new file on mousedown instead of click
    Closes #58

--- client/lib/menu-bar/dropdown.js
+++ client/lib/menu-bar/dropdown.js
@@ -9,31 +9,64 @@ function isButton(element) {
 }
 
 module.exports = function() {
-
   return {
     restrict: 'A',
-    scope: {},
     link: function(scope, element) {
 
-      function closeDropDown() {
-        element.removeClass('active');
+      // NOTE: THIS IS FOR ADDING THE ACTIVE STATE ONLZ
+      element.on("mouseenter", "li", function() {
+        console.log("mouse enter", this);
+        $(this).addClass("active");
+      });
+
+      element.on("mouseleave", "li", function() {
+        console.log("mouse leave", this);
+        $(this).removeClass("active");
+      });
+      // END NOTE
+
+      element.on("mouseup", "li", function(event) {
+        console.log("data-action", action);
+
+        var action = $(this).attr("data-action");
+
+        scope.$eval(action, { event: event });
+
+        close();
+      });
+
+      function open() {
+        $(document.body).on('mouseup', checkClose);
+        element.addClass("active");
       }
 
-      element.click(function(e) {
+      function close() {
+        element.removeClass('active');
+        $(document.body).off("mouseup", checkClose);
+      }
 
+      function checkClose(event) {
+        var target = $(event.target);
+
+        var insideDropDown = $(target).is("[dropdown]") || $(target).parents("[dropdown]").length;
+
+        if (insideDropDown) {
+          return;
+        }
+
+        close();
+      }
+
+      element.on("mousedown", function(e) {
         if (isButton(e.target)) {
           return;
         }
 
-        if (element.is('.active')) {
-          $(document.body).off('click', closeDropDown);
-        } else {
-          $(document.body).on('click', closeDropDown);
+        if (!element.is('.active')) {
+          open();
+        } else if (!$(e.target).is("li")) {
+          close();
         }
-
-        element.toggleClass('active');
-
-        e.stopPropagation();
       });
     }
   };
--- client/lib/menu-bar/index.js
+++ client/lib/menu-bar/index.js
@@ -1,6 +1,6 @@
 var angular = require('angular');
 
-var ngModule = module.exports = angular.module('app.menu-bar', [ ]);
+var ngModule = module.exports = angular.module('app.menu-bar', []);
 
 ngModule.directive('menuBar', require('./menuBar'));
 
