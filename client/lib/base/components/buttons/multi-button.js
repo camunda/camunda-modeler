459f894393a258739413dcefce71279d9ff9dc78
Ricardo Matias <ricardo.matias@camunda.com>
2016-02-25 17:40:16 +0100

    feat(multi-button): allow disabling of multi-buttons

--- client/lib/base/components/buttons/multi-button.js
+++ client/lib/base/components/buttons/multi-button.js
@@ -23,29 +23,39 @@ function MultiButton(options) {
 
   this.render = function() {
 
-    var primaryChoice = getPrimaryAction(this.choices);
+    var primaryChoice = getPrimaryAction(this.disabled, this.choices),
+        disabled = this.disabled ? 'disabled' : '',
+        dropdownWidget;
+
+    if (!this.disabled) {
+      dropdownWidget = (
+        <div className="dropdown-container">
+          <Separator />
+
+          <span className="caret"></span>
+
+          <ul className="dropdown">
+            {
+              this.choices.map(c => {
+                return <li className="entry" onMouseup={ c.action } ref={ c.id }>
+                  { c.label }
+                </li>;
+              })
+            }
+          </ul>
+        </div>
+      );
+    }
 
     var html =
-      <button className="multi-button" onMousedown={ dropdown() }>
+      <button className={ 'multi-button ' + disabled } onMousedown={ dropdown() }>
         <span className="primary"
               title={ primaryChoice.label || '' }
               onClick={ primaryChoice.action }>
           <span className={ primaryChoice.icon }></span>
         </span>
 
-        <Separator />
-
-        <span className="caret"></span>
-
-        <ul className="dropdown">
-          {
-            this.choices.map(c => {
-              return <li className="entry" onMouseup={ c.action } ref={ c.id }>
-                { c.label }
-              </li>;
-            })
-          }
-        </ul>
+        { dropdownWidget }
       </button>;
 
     return html;
@@ -57,13 +67,23 @@ inherits(MultiButton, BaseComponent);
 module.exports = MultiButton;
 
 
-function getPrimaryAction(choices) {
-  var primaryChoices = choices.filter(function(c) {
+function getPrimaryAction(isDisabled, choices) {
+  var primaryChoices;
+
+  primaryChoices = choices.filter(function(c) {
     return c.primary;
   });
 
   if (primaryChoices.length !== 1) {
     throw new Error('must define exactly one primary=true choice');
+  }
+
+  if (isDisabled) {
+    return {
+      label: primaryChoices[0].label,
+      icon: primaryChoices[0].icon,
+      action: function() {}
+    };
   }
 
   return primaryChoices[0];
