2d67b61e66bf0807857ad70f4bfb0a19d60b021b
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2018-03-23 21:09:24 +0100

    style(client): correct lint errors

--- client/lib/app/editor/dmn/CamundaDmnEditor.js
+++ client/lib/app/editor/dmn/CamundaDmnEditor.js
@@ -1,9 +1,3 @@
-import { is } from 'dmn-js-shared/lib/util/ModelUtil';
-
-import { containsDi } from 'dmn-js-shared/lib/util/DiUtil';
-
-import find from 'lodash/collection/find';
-
 import DmnJS from 'dmn-js/lib/Modeler';
 
 
