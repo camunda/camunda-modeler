5b964ead0e0324a8438e89efdafa90772db540b4
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2018-07-18 16:01:58 +0200

    feat(win): add file association support scripts
    Related to #787

---
+++ resources/platform/win32/support/README.md
@@ -0,0 +1,5 @@
+# Support Scripts
+
+Use the script `register_fileassoc.bat` in this directory
+to register the Camunda Modeler as the default editor
+for BPMN, CMMN and DMN files.
---
+++ resources/platform/win32/support/register_fileassoc.bat
@@ -0,0 +1,34 @@
+@echo off
+setlocal enabledelayedexpansion
+set DIR=%~dp0
+
+set IN_FILE=%DIR%\fileassoc.reg
+set OUT_FILE=%DIR%\fileassoc_full_path.reg
+set PLACEHOLDER=__CWD__
+
+echo %IN_FILE%
+
+rem move one level up
+cd %DIR%
+cd ..
+
+set DIR=%cd%
+
+echo %DIR%
+set EXECUTABLE_DIR=!DIR:\=\\!
+set EXECUTABLE_PATH=%EXECUTABLE_DIR%\\Camunda Modeler.exe
+
+echo "%EXECUTABLE_PATH%"
+
+set OUTPUTLINE=
+
+for /f "tokens=1,* delims=¶" %%A in ( '"type %IN_FILE%"') do (
+	SET string=%%A
+
+	SET modified=!string:%PLACEHOLDER%=%EXECUTABLE_PATH%!
+	echo !modified! >> %OUT_FILE%
+)
+
+reg import %OUT_FILE%
+
+del %OUT_FILE%
