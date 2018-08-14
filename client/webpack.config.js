d7c3bc833df06a2bfedd3d8dbf87e6256456cb7c
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2018-08-14 11:21:14 +0200

    chore(client): create simple webpack config
    * drop create-react-app due to bloat
    * add simple webpack setup that serves the same need

---
+++ client/webpack.config.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const NODE_ENV = process.env.NODE_ENV;
+const DEV = NODE_ENV !== 'production';
+
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
+module.exports = {
+  mode: NODE_ENV,
+  entry: {
+    bundle: ['./src/index.js']
+  },
+  output: {
+    path: __dirname + '/build',
+    filename: '[name].js',
+    chunkFilename: '[name].[id].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      // apply loaders, falling back to file-loader, if non matches
+      {
+        oneOf: [
+          {
+            test: /\.css$/,
+            use: [
+              'style-loader',
+              'css-loader'
+            ]
+          },
+          {
+            // exclude files served otherwise
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            loader: 'file-loader',
+            options: {
+              name: 'static/media/[name].[hash:8].[ext]',
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CaseSensitivePathsPlugin(),
+    new CopyWebpackPlugin([
+      { from: './public' }
+    ])
+  ],
+  // ship source map during development only
+  devtool: DEV && 'cheap-module-source-map',
+  // don't bundle shims for node globals
+  node: {
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty',
+  }
+};
