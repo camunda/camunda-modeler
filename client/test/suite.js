ac719d691bd842ca86b10f6c04e0ec144118ea61
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2018-08-30 08:54:56 +0200

    test(client): recognize tests in src/*/__tests__

--- client/test/suite.js
+++ client/test/suite.js
@@ -3,6 +3,10 @@ import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
+var scopedTests = require.context('../src', true, /\/__tests__\/.*Spec\.js$/);
+
+scopedTests.keys().forEach(scopedTests);
+
 var allTests = require.context('.', true, /(spec|integration).*Spec\.js$/);
 
 allTests.keys().forEach(allTests);
