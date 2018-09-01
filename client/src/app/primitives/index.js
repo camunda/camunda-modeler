a58c0dc68e6e507b0852bdca5fa49a31a3f8136b
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2018-09-01 12:58:02 +0200

    feat(client): add Loader primitive

--- client/src/app/primitives/index.js
+++ client/src/app/primitives/index.js
@@ -1,6 +1,7 @@
 export { default as Button } from './Button';
 export { default as DropdownButton } from './DropdownButton';
 export { Icon } from './icon';
+export { default as Loader } from './Loader';
 export { default as TabLinks } from './TabLinks';
 export { default as TabContainer } from './TabContainer';
 export { default as Tab } from './Tab';
