aaf4cd432ef5d3aef5f13e49c2a4dae25c8f40d9
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2018-09-01 12:55:36 +0200

    feat(client): add Icon primitive

--- client/src/app/primitives/index.js
+++ client/src/app/primitives/index.js
@@ -1,5 +1,6 @@
 export { default as Button } from './Button';
 export { default as DropdownButton } from './DropdownButton';
+export { Icon } from './icon';
 export { default as TabLinks } from './TabLinks';
 export { default as TabContainer } from './TabContainer';
 export { default as Tab } from './Tab';
