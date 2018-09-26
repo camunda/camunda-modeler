3594ceb4daf9aa9cf0a84918a6f3c707aa991611
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2018-09-26 20:31:35 +0200

    feat(client/App): restore empty tab behavior
    Closes #919

--- client/src/app/EmptyTab.js
+++ client/src/app/EmptyTab.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 
 import Slot from './slot-fill/Slot';
 
+import css from './EmptyTab.less';
+
 import {
   Tab
 } from './primitives';
+
 
 export default class EmptyTab extends Component {
 
@@ -13,8 +16,22 @@ export default class EmptyTab extends Component {
   }
 
   render() {
+
+    const {
+      onAction
+    } = this.props;
+
     return (
-      <Tab className="tab">
+      <Tab className={ css.EmptyTab }>
+        <p className="create-buttons">
+          <span>Create a </span>
+          <button className="create-bpmn" onClick={ () => onAction('create-bpmn-diagram') }>BPMN diagram</button>
+          <span> or </span>
+          <button onClick={ () => onAction('create-dmn-diagram') }>DMN diagram</button>
+          <span> or </span>
+          <button onClick={ () => onAction('create-cmmn-diagram') }>CMMN diagram</button>
+        </p>
+
         <Slot name="empty-tab-buttons" />
       </Tab>
     );
