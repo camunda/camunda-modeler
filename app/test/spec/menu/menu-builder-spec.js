141d165c6aa07b55a38c799adb77008793f4eafd
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2018-11-12 10:34:46 +0100

    test(menu): add basic tests with proxyquire

---
+++ app/test/spec/menu/menu-builder-spec.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const proxyquire = require('proxyquire');
+
+const ElectronApp = require('../../helper/mock/electron-app');
+const ElectronMenu = require('../../helper/mock/electron-menu');
+const ElectronMenuItem = require('../../helper/mock/electron-menu-item');
+
+const Electron = {
+  app: ElectronApp,
+  Menu: ElectronMenu,
+  MenuItem: ElectronMenuItem
+};
+
+const MenuBuilder = proxyquire('../../../lib/menu/menu-builder.js', {
+  electron: Electron
+});
+
+
+describe('MenuBuilder', () => {
+
+  it('should instantiate', () => {
+    const menuBuilder = new MenuBuilder();
+
+    expect(menuBuilder).to.exist;
+  });
+
+
+  it('should build menu', () => {
+    const menuBuilder = new MenuBuilder();
+
+    const menu = menuBuilder.build();
+
+    expect(menu).to.exist;
+  });
+
+});
