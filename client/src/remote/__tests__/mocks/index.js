210e492f7973662f280ba512552b570066e3fab4
Philipp Fromme <philippfromme@outlook.com>
2018-11-14 09:26:01 +0100

    fix(app/client): fix ipcRenderer communication

---
+++ client/src/remote/__tests__/mocks/index.js
@@ -0,0 +1,27 @@
+export class IpcRenderer {
+  constructor() {
+    this.response = null;
+
+    this.listener = null;
+  }
+
+  setSendResponse(response) {
+    this.sendResponse = response;
+  }
+
+  send(event, id, args) {
+    this.listener(null, this.sendResponse);
+  }
+
+  on(event, callback) {
+    this.listener = callback;
+  }
+
+  off(event) {
+    this.listener = null;
+  }
+
+  once(event, callback) {
+    this.listener = callback;
+  }
+}
