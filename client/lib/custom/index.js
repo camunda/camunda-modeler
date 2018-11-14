6aea4ea2b3e117d53298adf0bb9ac3ecc5afb213
Dominik Horn <dominik.horn93@gmail.com>
2018-11-14 23:56:36 +0100

    Fix issues #21

--- client/lib/custom/index.js
+++ client/lib/custom/index.js
@@ -1,7 +1,6 @@
 module.exports = {
-  __init__: [  'paletteProvider', 'customRules', 'contextPadProvider', 'replaceMenuProvider' ],
+  __init__: [  'paletteProvider', 'contextPadProvider', 'replaceMenuProvider' ],
   paletteProvider: [ 'type', require('./CustomPalette') ],
-  customRules: [ 'type', require('./CustomRules') ],
   replaceMenuProvider: [ 'type', require('./CustomReplaceMenuProvider') ],
   contextPadProvider: [ 'type', require('./CustomContextPadProvider') ]
 };
