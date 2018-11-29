dfb007a94b93ceea863e57c2d1acf8a954144d69
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2018-11-29 15:45:25 +0100

    feat(deploy-toolbar): remember last endpoint url

--- client/src/app/__tests__/DeployDiagramModalSpec.js
+++ client/src/app/__tests__/DeployDiagramModalSpec.js
@@ -33,7 +33,7 @@ describe('<DeployDiagramModal>', function() {
   });
 
 
-  it('should set state.success when onDeploy resolves', async function() {
+  it('should set state.success when onDeploy succeeds', async function() {
     // given
     const endpointUrl = 'http://example.com';
 
@@ -50,6 +50,51 @@ describe('<DeployDiagramModal>', function() {
     expect(instance.state.success).to.be.a('string').not.eql('');
     expect(instance.state.error).to.eql('');
     expect(instance.state.isLoading).to.be.false;
+  });
+
+
+  it('should save endpoint used to deploy', async function() {
+    // given
+    const endpointUrl = 'http://example.com';
+
+    const onDeployStub = sinon.stub().resolves();
+    const onEndpointsUpdateSpy = sinon.spy();
+
+    const wrapper = shallow(
+      <DeployDiagramModal
+        onDeploy={ onDeployStub }
+        onEndpointsUpdate={ onEndpointsUpdateSpy }
+      />
+    );
+    const instance = wrapper.instance();
+    instance.state.endpointUrl = endpointUrl;
+
+    // when
+    await instance.handleDeploy(new Event('click'));
+
+    // expect
+    expect(onEndpointsUpdateSpy).to.be.calledWith([ endpointUrl ]);
+  });
+
+
+  it('should set endpointUrl to last one provided in props', async function() {
+    // given
+    const endpointUrl = 'http://example.com';
+
+    // when
+    const wrapper = shallow(<DeployDiagramModal endpoints={ [ endpointUrl ] } />);
+
+    // expect
+    expect(wrapper.state('endpointUrl')).to.eql(endpointUrl);
+  });
+
+
+  it('should set endpointUrl to void string when there is none provided', async function() {
+    // given
+    const wrapper = shallow(<DeployDiagramModal />);
+
+    // expect
+    expect(wrapper.state('endpointUrl')).to.eql('');
   });
 
 });
