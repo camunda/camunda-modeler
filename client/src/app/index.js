0d285c28a4b813a7edd3f48b57ed709f192e5adf
Philipp Fromme <philippfromme@outlook.com>
2018-12-05 11:57:39 +0100

    feat(client): add KeyboardBindings

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,3 +1,4 @@
 export { default as App } from './App';
 export { default as AppParent } from './AppParent';
+export { default as KeyboardBindings } from './KeyboardBindings';
 export { default as TabsProvider } from './TabsProvider';
