e551f3efb347ea211dc945b545de8e043c8e94ab
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2019-01-17 09:33:42 +0100

    chore(project): remove client2

--- client2/lib/util/has-property.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var hasProp = Object.prototype.hasOwnProperty;
-
-
-module.exports = function hasProperty(object, propName) {
-  return hasProp.call(object, propName);
-};
--- client2/lib/vdom/create-component.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = function createComponent(Constructor, options, children) {
-  return new Constructor(options, children);
-};
