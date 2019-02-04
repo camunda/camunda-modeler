dd42ab40afcf8bbe5a08d30f24138d13c103caa1
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2019-02-04 12:58:17 +0100

    chore(app): add generic file globbing + parsing utility

---
+++ app/lib/util/__tests__/files-spec.js
@@ -0,0 +1,114 @@
+const path = require('path');
+
+const {
+  globJSON,
+  globFiles
+} = require('../files');
+
+
+describe('files', function() {
+
+  describe('#globJSON', function() {
+
+    it('should load by name', function() {
+
+      // when
+      const { config } = globJSON({
+        name: 'config.json',
+        searchPaths: [
+          absPath('files/bar')
+        ]
+      });
+
+      // then
+      expect(config).to.eql({ bar: 'BAR' });
+    });
+
+
+    it('should load by pattern', function() {
+
+      // when
+      const { config } = globJSON({
+        name: '**/config.json',
+        searchPaths: [
+          absPath('files')
+        ]
+      });
+
+      // then
+      expect(config).to.eql({
+        foo: 'FOO',
+        bar: 'FOO OVERRIDE'
+      });
+    });
+
+
+    it('should consider defaults', function() {
+
+      // when
+      const { config } = globJSON({
+        name: 'config.json',
+        searchPaths: [
+          absPath('files/bar')
+        ],
+        defaults: {
+          foo: 'default FOO',
+          bar: 'default BAR'
+        }
+      });
+
+      // then
+      expect(config).to.eql({
+        foo: 'default FOO',
+        bar: 'BAR'
+      });
+    });
+
+
+    it('should handle errors gracefully', function() {
+
+      // when
+      const { config } = globJSON({
+        name: '**/*-json.json',
+        searchPaths: [
+          absPath('files')
+        ]
+      });
+
+      // then
+      expect(config).to.eql({
+        good: 'JSON'
+      });
+    });
+
+  });
+
+
+  describe('#globFiles', function() {
+
+    it('should find files by pattern', function() {
+
+      // when
+      const files = globFiles({
+        pattern: '**/*-json.json',
+        searchPaths: [
+          absPath('files')
+        ]
+      });
+
+      // then
+      expect(files).to.eql([
+        absPath('files/bar/good-json.json'),
+        absPath('files/foo/not-json.json')
+      ]);
+
+    });
+
+  });
+
+});
+
+
+function absPath(file) {
+  return path.resolve(__dirname, file);
+}
---
+++ app/lib/util/files.js
@@ -0,0 +1,133 @@
+const glob = require('glob');
+
+const fs = require('fs');
+
+/**
+ * Load JSON configuration from multiple paths.
+ *
+ * @example
+ *
+ * const {
+ *   errors,
+ *   files,
+ *   config
+ * } = globJSON({
+ *   name: 'config.json',
+ *   searchPaths: [ __dirname, process.cwd() ]
+ * });
+ *
+ * @return { config, files, errors }
+ */
+function globJSON(options) {
+
+  const {
+    name,
+    pattern,
+    searchPaths,
+    defaults
+  } = options;
+
+  const files = globFiles({
+    searchPaths,
+    pattern: name || pattern
+  });
+
+  const {
+    contents,
+    errors
+  } = readJSON(files);
+
+  const config = merge(defaults, ...contents);
+
+  return {
+    config,
+    files,
+    errors
+  };
+}
+
+module.exports.globJSON = globJSON;
+
+
+function merge(...objects) {
+  return Object.assign({}, ...objects);
+}
+
+/**
+ * Read the given files and return { contents: [], errors: [] }.
+ *
+ * @param  {Array<String>} files
+ *
+ * @return {Object}
+ */
+function readJSON(files) {
+  return files.reduce((results, file) => {
+
+    const {
+      errors,
+      contents
+    } = results;
+
+    try {
+      const result = readFileAsJSON(file);
+
+      return {
+        errors,
+        contents: [ ...contents, result ]
+      };
+    } catch (error) {
+      return {
+        errors: [ ...errors, error ],
+        contents
+      };
+    }
+  }, { errors: [], contents: [] });
+}
+
+module.exports.readJSON = readJSON;
+
+
+function readFileAsJSON(file) {
+
+  try {
+    const contents = fs.readFileSync(file, { encoding: 'UTF-8' });
+
+    return JSON.parse(contents);
+  } catch (error) {
+    throw new Error(`failed to process ${file}: ${error.message}`);
+  }
+}
+
+/**
+ * Find files by pattern in a list of search paths.
+ *
+ * @param {Object} options
+ * @param {Array<String>} options.searchPaths
+ * @param {String} options.pattern
+ *
+ * @return {Array<String>} list of found, absolute path names
+ */
+function globFiles(options) {
+
+  const {
+    searchPaths,
+    pattern
+  } = options;
+
+  return searchPaths.reduce((paths, searchPath) => {
+
+    const newPaths = glob.sync(pattern, {
+      cwd: searchPath,
+      nodir: true,
+      realpath: true
+    });
+
+    return [
+      ...paths,
+      ...newPaths
+    ];
+  }, []);
+
+}
+
+module.exports.globFiles = globFiles;
