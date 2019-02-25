634f1559d1e731ef6b4b7ac2ad94ab9a01815a33
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2019-02-25 14:44:02 +0100

    fix(tasks/after-pack): properly wait for handler completion

--- tasks/after-pack.js
+++ tasks/after-pack.js
@@ -12,7 +12,9 @@ const handlers = [
 
 
 async function afterPack(context) {
-  return await handlers.map(h => h(context));
+  return Promise.all(
+    handlers.map(h => h(context))
+  );
 }
 
 module.exports = afterPack;
