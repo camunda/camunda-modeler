6a782e0801766277671f976919d229c98ea4fa68
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2019-02-27 16:01:19 +0100

    chore(modals/keyboard-shortcuts): fetch platform via backend

--- client/src/app/modals/keyboard-shortcuts/KeyboardShortcutsModal.js
+++ client/src/app/modals/keyboard-shortcuts/KeyboardShortcutsModal.js
@@ -16,7 +16,9 @@ import getShortcuts from './getShortcuts';
 
 class KeyboardShortcutsModal extends PureComponent {
   getModifierKey() {
-    return this.props.isMac ? 'Command' : 'Control';
+    const platform = this.props.getGlobal('backend').getPlatform();
+
+    return platform === 'darwin' ? 'Command' : 'Control';
   }
 
   render() {
