229dbfdb5ad0c7be5a97bf371ec91c09d4434d33
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2019-07-31 19:15:16 +0200

    feat(app): verify we are opening actual urls externally

--- app/lib/util/browser-open.js
+++ app/lib/util/browser-open.js
@@ -11,5 +11,11 @@
 const { shell } = require('electron');
 
 module.exports = function(url) {
+
+  // as a security best practice, prevent opening of non-url links
+  if (!/^https?:\/\//.test(url)) {
+    return;
+  }
+
   return shell.openExternal(url);
 };
