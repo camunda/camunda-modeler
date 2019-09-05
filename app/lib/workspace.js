e64421c1dd2db9f0e4366b2af4499b8655d361c3
Philipp Fromme <philippfromme@outlook.com>
2019-09-05 15:59:00 +0200

    chore(workspace): make class

--- app/lib/workspace.js
+++ app/lib/workspace.js
@@ -17,47 +17,48 @@ const log = require('./log')('app:workspace');
 const { forEach } = require('min-dash');
 
 
-function Workspace(config) {
+class Workspace {
+  constructor(config) {
+    renderer.on('workspace:restore', (defaultConfig, done) => {
+      const workspace = config.get('workspace', null);
 
-  renderer.on('workspace:restore', function(defaultConfig, done) {
-    var files = [],
-        workspace = config.get('workspace', null);
-
-    if (!workspace) {
-      return done(null, defaultConfig);
-    }
-
-    // ensure backwards compatibility
-    forEach((workspace.files || workspace.tabs), function(diagram) {
-      const {
-        path
-      } = diagram;
-
-      try {
-        log.info('restoring %s', path);
-
-        files.push(readFile(path));
-
-      } catch (err) {
-        log.error('failed to restore %s', path, err);
+      if (!workspace) {
+        return done(null, defaultConfig);
       }
+
+      let files = [];
+
+      // ensure backwards compatibility
+      forEach((workspace.files || workspace.tabs), file => {
+        const { path } = file;
+
+        try {
+          log.info(`restoring ${ path }`);
+
+          files = [
+            ...files,
+            readFile(path)
+          ];
+        } catch (error) {
+          log.error(`failed to restore ${ path }`, error);
+        }
+      });
+
+      workspace.files = files;
+
+      workspace.endpoints = workspace.endpoints || [];
+
+      done(null, workspace);
     });
 
-    workspace.files = files;
+    renderer.on('workspace:save', (workspace, done) => {
+      log.info('saving');
 
-    workspace.endpoints = workspace.endpoints || [];
+      config.set('workspace', workspace);
 
-    done(null, workspace);
-  });
-
-  renderer.on('workspace:save', function(workspace, done) {
-
-    log.info('saving');
-
-    config.set('workspace', workspace);
-
-    done(null);
-  });
+      done(null);
+    });
+  }
 }
 
 module.exports = Workspace;
