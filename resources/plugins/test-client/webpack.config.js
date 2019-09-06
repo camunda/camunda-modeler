e7c482a3392e3b53c0a702a481e207f928b066e8
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2019-09-06 15:00:11 +0200

    chore(project): add test-client plug-in

---
+++ resources/plugins/test-client/webpack.config.js
@@ -0,0 +1,35 @@
+/**
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.
+ *
+ * Camunda licenses this file to you under the MIT; you may not use this file
+ * except in compliance with the MIT License.
+ */
+
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'client.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'cheap-module-source-map'
+};
