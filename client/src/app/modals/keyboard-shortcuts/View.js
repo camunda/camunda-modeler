41f9a3080eeee09af96200ef3429b91845d03aa1
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2019-10-24 15:32:20 +0200

    feat(keyboard-shortcuts): use modal structural components

--- client/src/app/modals/keyboard-shortcuts/View.js
+++ client/src/app/modals/keyboard-shortcuts/View.js
@@ -26,25 +26,36 @@ class View extends PureComponent {
 
     return (
       <Modal className={ css.View } onClose={ onClose }>
-        <h2>Keyboard Shortcuts</h2>
-        <p>
-          The following special shortcuts can be used on opened diagrams.
-        </p>
-        <table>
-          <tbody className="keyboard-shortcuts">
-            {
-              (shortcuts || []).map(s => {
-                return <tr key={ s.id }>
-                  <td>{ s.label }</td>
-                  <td className="binding"><code>{ s.binding }</code></td>
-                </tr>;
-              })
-            }
-          </tbody>
-        </table>
-        <p>
-          Find additional shortcuts on individual items in the application menu.
-        </p>
+
+        <Modal.Header>
+          <Modal.Title>Keyboard Shortcuts</Modal.Title>
+          <Modal.Close onClick={ onClose } />
+        </Modal.Header>
+
+        <Modal.Body>
+          <p>
+            The following special shortcuts can be used on opened diagrams.
+          </p>
+          <table>
+            <tbody className="keyboard-shortcuts">
+              {
+                (shortcuts || []).map(s => {
+                  return <tr key={ s.id }>
+                    <td>{ s.label }</td>
+                    <td className="binding"><code>{ s.binding }</code></td>
+                  </tr>;
+                })
+              }
+            </tbody>
+          </table>
+          <p>
+            Find additional shortcuts on individual items in the application menu.
+          </p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <button onClick={ onClose }>Close</button>
+        </Modal.Footer>
       </Modal>
     );
   }
