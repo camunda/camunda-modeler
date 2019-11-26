22ab13c045ab9be5666f7d46f3375d1e0dcfcd8a
Oguz Eroglu <oguz.eroglu@camunda.com>
2019-11-26 17:43:27 +0100

    feat(app): OsInfoProvider implementation

--- app/lib/config/index.js
+++ app/lib/config/index.js
@@ -13,6 +13,7 @@ const path = require('path');
 const DefaultProvider = require('./providers/DefaultProvider');
 const ElementTemplatesProvider = require('./providers/ElementTemplatesProvider');
 const UUIDProvider = require('./providers/UUIDProvider');
+const OSInfoProvider = require('./providers/OSInfoProvider');
 
 const { isFunction } = require('min-dash');
 
@@ -31,7 +32,8 @@ class Config {
 
     this._providers = {
       'bpmn.elementTemplates': new ElementTemplatesProvider(resourcesPaths),
-      'editor.id': new UUIDProvider(path.join(userPath, '.editorid'))
+      'editor.id': new UUIDProvider(path.join(userPath, '.editorid')),
+      'os.info': new OSInfoProvider()
     };
   }
 
