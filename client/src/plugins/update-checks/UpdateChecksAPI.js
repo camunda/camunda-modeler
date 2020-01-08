62b2f093ad18894fa89ea062ee328b97688dd1ec
Oguz Eroglu <oguz.eroglu@camunda.com>
2020-01-08 16:49:20 +0100

    feat(client): UpdateChecks related flags

--- client/src/plugins/update-checks/UpdateChecksAPI.js
+++ client/src/plugins/update-checks/UpdateChecksAPI.js
@@ -14,9 +14,19 @@ const EDITOR_ID_CONFIG_KEY = 'editor.id';
 const OS_INFO_CONFIG_KEY = 'os.info';
 
 const UPDATE_CHECK_API = 'update-check';
-const UPDATES_SERVER_URL = 'https://camunda-modeler-updates.camunda.com/';
+const PROD_UPDATES_SERVER_URL = 'https://camunda-modeler-updates.camunda.com/';
+const DEV_UPDATES_SERVER_URL = 'http://camunda-modeler-update-server-staging.camunda.com';
 
 export default class UpdateChecksAPI {
+
+  constructor(updatesServerURLFlagValue) {
+
+    if (updatesServerURLFlagValue) {
+      this.updatesServerURL = updatesServerURLFlagValue;
+    } else {
+      this.updatesServerURL = __ENVIRONMENT__ === 'development' ? DEV_UPDATES_SERVER_URL : PROD_UPDATES_SERVER_URL;
+    }
+  }
 
   async sendRequest(url) {
     const response = await fetch(url, { method: 'GET' });
@@ -41,7 +51,7 @@ export default class UpdateChecksAPI {
       const osInfo = await config.get(OS_INFO_CONFIG_KEY);
       const plugins = this.formatPlugins(getGlobal('plugins').appPlugins);
 
-      const url = new URL(UPDATE_CHECK_API, UPDATES_SERVER_URL);
+      const url = new URL(UPDATE_CHECK_API, this.updatesServerURL);
       url.searchParams.append('editorID', editorID);
       url.searchParams.append('newerThan', newerThan);
       url.searchParams.append('modelerVersion', modelerVersion);
