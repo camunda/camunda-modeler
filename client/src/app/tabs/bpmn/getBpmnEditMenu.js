b8eca509fdfa535f0dbca54c079051b3225cd093
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2020-02-10 22:02:10 +0100

    chore(client): make align/distribute menu entries shared

--- client/src/app/tabs/bpmn/getBpmnEditMenu.js
+++ client/src/app/tabs/bpmn/getBpmnEditMenu.js
@@ -10,6 +10,7 @@
 
 import {
   getCanvasEntries,
+  getAlignDistributeEntries,
   getCopyCutPasteEntries,
   getDefaultCopyCutPasteEntries,
   getDefaultUndoRedoEntries,
@@ -19,43 +20,6 @@ import {
   getUndoRedoEntries
 } from '../getEditMenu';
 
-function getAlignDistributeEntries({
-  align,
-  distribute
-}) {
-  return [{
-    label: 'Align Elements',
-    enabled: align,
-    submenu: [ 'Left', 'Right', 'Center', 'Top', 'Bottom', 'Middle' ].map(direction => {
-      return {
-        label: `Align ${direction}`,
-        enabled: align,
-        action: 'alignElements',
-        options: {
-          type: direction.toLowerCase()
-        }
-      };
-    })
-  }, {
-    label: 'Distribute Elements',
-    enabled: distribute,
-    submenu: [{
-      label: 'Distribute Horizontally',
-      enabled: distribute,
-      action: 'distributeElements',
-      options: {
-        type: 'horizontal'
-      }
-    }, {
-      label: 'Distribute Vertically',
-      enabled: distribute,
-      action: 'distributeElements',
-      options: {
-        type: 'vertical'
-      }
-    }]
-  }];
-}
 
 export function getBpmnEditMenu(state) {
   const {
