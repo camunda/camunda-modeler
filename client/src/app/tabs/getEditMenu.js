23ce5e21dd6d61248d610a5488ca5f0fcf3739f9
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2020-02-10 22:02:10 +0100

    chore(client): make align/distribute menu entries shared

--- client/src/app/tabs/getEditMenu.js
+++ client/src/app/tabs/getEditMenu.js
@@ -12,6 +12,44 @@ import Flags, { DISABLE_ADJUST_ORIGIN } from '../../util/Flags';
 
 const SPACE_KEY = ' ';
 
+export function getAlignDistributeEntries({
+  align,
+  distribute
+}) {
+  return [{
+    label: 'Align Elements',
+    enabled: align,
+    submenu: [ 'Left', 'Right', 'Center', 'Top', 'Bottom', 'Middle' ].map(direction => {
+      return {
+        label: `Align ${direction}`,
+        enabled: align,
+        action: 'alignElements',
+        options: {
+          type: direction.toLowerCase()
+        }
+      };
+    })
+  }, {
+    label: 'Distribute Elements',
+    enabled: distribute,
+    submenu: [{
+      label: 'Distribute Horizontally',
+      enabled: distribute,
+      action: 'distributeElements',
+      options: {
+        type: 'horizontal'
+      }
+    }, {
+      label: 'Distribute Vertically',
+      enabled: distribute,
+      action: 'distributeElements',
+      options: {
+        type: 'vertical'
+      }
+    }]
+  }];
+}
+
 export function getCanvasEntries({
   moveCanvas,
   moveToOrigin,
