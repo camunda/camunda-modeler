1dd52a36066677257535325fa3d9c05754e957d8
Oguz <eroglu.oguz93@gmail.com>
2020-03-05 19:22:54 +0100

    feat(client): update UI component classes based on new designs

--- client/src/plugins/camunda-plugin/shared/components/index.js
+++ client/src/plugins/camunda-plugin/shared/components/index.js
@@ -11,3 +11,4 @@
 export { default as TextInput } from './TextInput';
 export { default as CheckBox } from './CheckBox';
 export { default as Select } from './Select';
+export { default as Radio } from './Radio';
