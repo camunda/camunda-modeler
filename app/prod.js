6c8734778be483fb26a26b868762344fdd60d464
Oguz <eroglu.oguz93@gmail.com>
2020-05-12 17:35:41 +0200

    feat(app): inject SENTRY_DSN into backend

--- app/prod.js
+++ app/prod.js
@@ -8,7 +8,12 @@
  * except in compliance with the MIT License.
  */
 
+ const pkg = require('./package');
+
 // ensure production mode
 process.env.NODE_ENV = 'production';
+
+// Injected to pkg via electron-builder extraMetadata configuration.
+process.env.SENTRY_DSN = pkg.SENTRY_DSN;
 
 require('./lib');
