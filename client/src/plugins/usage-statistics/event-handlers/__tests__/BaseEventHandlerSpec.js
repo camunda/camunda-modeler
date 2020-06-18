c1637ab8436e5fc55b21d242de9d737f6314f3c1
Oguz <eroglu.oguz93@gmail.com>
2020-06-18 10:35:14 +0200

    feat(usage-statistics): base event handler

---
+++ client/src/plugins/usage-statistics/event-handlers/__tests__/BaseEventHandlerSpec.js
@@ -0,0 +1,114 @@
+/**
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.
+ *
+ * Camunda licenses this file to you under the MIT; you may not use this file
+ * except in compliance with the MIT License.
+ */
+
+/* global sinon */
+
+import BaseEventHandler from '../BaseEventHandler';
+
+const noop = () => {};
+
+describe('<BaseEventHandler>', () => {
+
+  it('should enable', () => {
+
+    // given
+    const baseEventHandler = new BaseEventHandler('testEvent', noop);
+
+    // when
+    baseEventHandler.enable();
+
+    // then
+    expect(baseEventHandler.isEnabled()).to.eql(true);
+  });
+
+
+  it('should disable', () => {
+
+    // given
+    const baseEventHandler = new BaseEventHandler('testEvent', noop);
+
+    // when
+    baseEventHandler.enable();
+    baseEventHandler.disable();
+
+    // then
+    expect(baseEventHandler.isEnabled()).to.eql(false);
+  });
+
+
+  it('should execute onAfterEnable', () => {
+
+    // given
+    const baseEventHandler = new BaseEventHandler('testEvent', noop);
+
+    const onAfterEnableSpy = sinon.spy();
+
+    baseEventHandler.onAfterEnable = onAfterEnableSpy;
+
+    // when
+    baseEventHandler.enable();
+
+    // then
+    expect(onAfterEnableSpy).to.have.been.calledOnce;
+  });
+
+
+  it('should execute onAfterDisable', () => {
+
+    // given
+    const baseEventHandler = new BaseEventHandler('testEvent', noop);
+
+    const onAfterDisableSpy = sinon.spy();
+
+    baseEventHandler.onAfterDisable = onAfterDisableSpy;
+
+    // when
+    baseEventHandler.enable();
+    baseEventHandler.disable();
+
+    // then
+    expect(onAfterDisableSpy).to.have.been.calledOnce;
+  });
+
+
+  it('should send to ET', () => {
+
+    // given
+    const sendSpy = sinon.spy();
+
+    const baseEventHandler = new BaseEventHandler('testEvent', sendSpy);
+
+    // when
+    baseEventHandler.enable();
+
+    baseEventHandler.sendToET({ testData: true });
+
+    // then
+    expect(sendSpy).to.have.been.calledWith({
+      event: 'testEvent',
+      testData: true
+    });
+  });
+
+
+  it('should not send if not enabled', () => {
+
+    // given
+    const sendSpy = sinon.spy();
+
+    const baseEventHandler = new BaseEventHandler('testEvent', sendSpy);
+
+    // when
+    baseEventHandler.sendToET({ testData: true });
+
+    // then
+    expect(sendSpy).to.not.have.been.called;
+  });
+});
