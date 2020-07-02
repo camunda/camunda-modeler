36932ac58f6bff350989d8a7264eca0aeaa9e2ef
Philipp Fromme <philippfromme@outlook.com>
2020-07-02 09:27:43 +0200

    feat(usage-statistics): include element templates in diagramOpened event
    Closes #1850

--- client/src/plugins/usage-statistics/event-handlers/DiagramOpenEventHandler.js
+++ client/src/plugins/usage-statistics/event-handlers/DiagramOpenEventHandler.js
@@ -10,6 +10,11 @@
 
 import BaseEventHandler from './BaseEventHandler';
 
+const HTTP_STATUS_PAYLOAD_TOO_BIG = 413;
+
+const BINDING_TYPE_PROPERTY = 'property';
+const ELEMENT_TEMPLATES_CONFIG_KEY = 'bpmn.elementTemplates';
+
 const types = {
   BPMN: 'bpmn',
   DMN: 'dmn'
@@ -21,12 +26,18 @@ export default class DiagramOpenEventHandler extends BaseEventHandler {
 
   constructor(params) {
 
-    const { onSend, subscribe } = params;
+    const { onSend, subscribe, config } = params;
 
     super('diagramOpened', onSend);
 
-    subscribe('bpmn.modeler.created', () => {
-      this.onDiagramOpened(types.BPMN);
+    subscribe('bpmn.modeler.created', async (info) => {
+
+      const { tab } = info;
+      const { file } = tab;
+
+      const elementTemplates = await this.getElementTemplates(config, file);
+
+      this.onDiagramOpened(types.BPMN, elementTemplates);
     });
 
     subscribe('dmn.modeler.created', () => {
@@ -34,7 +45,66 @@ export default class DiagramOpenEventHandler extends BaseEventHandler {
     });
   }
 
-  onDiagramOpened = (type) => {
-    this.sendToET({ 'diagram-type': type });
+  onDiagramOpened = async (type, elementTemplates) => {
+
+    if (!this.isEnabled()) {
+      return;
+    }
+
+    const payload = { 'diagram-type': type };
+
+    if (elementTemplates) {
+      payload.elementTemplates = elementTemplates;
+      payload.elementTemplateCount = elementTemplates.length;
+    }
+
+    const response = await this.sendToET(payload);
+
+    if (response.status === HTTP_STATUS_PAYLOAD_TOO_BIG) {
+
+      // Payload too large, send again with smaller payload
+      this.sendToET({
+        'diagram-type': type,
+        elementTemplateCount: payload.elementTemplateCount
+      });
+    }
+  }
+
+  getElementTemplates = async (config, file) => {
+
+    const elementTemplates = await config.get(ELEMENT_TEMPLATES_CONFIG_KEY, file);
+
+    if (!elementTemplates) {
+      return [];
+    }
+
+    return elementTemplates.map((elementTemplate) => {
+      const { appliesTo, properties } = elementTemplate;
+
+      const propertyCounts = properties.map((property) => {
+
+        const { binding } = property;
+        const { type, name } = binding;
+
+        if (type === BINDING_TYPE_PROPERTY) {
+          return name;
+        }
+
+        return type;
+      }).reduce((propertyCounts, property) => {
+
+        if (propertyCounts[ property ]) {
+
+          propertyCounts[ property ]++;
+        } else {
+
+          propertyCounts[ property ] = 1;
+        }
+
+        return propertyCounts;
+      }, {});
+
+      return { appliesTo, properties: propertyCounts };
+    });
   }
 }
