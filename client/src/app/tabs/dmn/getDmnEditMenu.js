24211add3726952628cc285e2da96c3498ee8a21
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2020-07-31 13:16:58 +0200

    feat(dmn): remove clause and rule menu entries
    Closes #1901

--- client/src/app/tabs/dmn/getDmnEditMenu.js
+++ client/src/app/tabs/dmn/getDmnEditMenu.js
@@ -26,55 +26,6 @@ function getDecisionTableEntries({
   return [
     [
       {
-        label: 'Add Rule',
-        submenu: [{
-          label: 'At End',
-          accelerator: 'CommandOrControl+D',
-          action: 'addRule'
-        }, {
-          label: 'Above Selected',
-          enabled: hasSelection,
-          action: 'addRuleAbove'
-        }, {
-          label: 'Below Selected',
-          enabled: hasSelection,
-          action: 'addRuleBelow'
-        }]
-      }, {
-        label: 'Remove Rule',
-        enabled: hasSelection,
-        action: 'removeRule'
-      }
-    ], [
-      {
-        label: 'Add Clause',
-        submenu: [{
-          label: 'Input',
-          action: 'addInput'
-        }, {
-          label: 'Output',
-          action: 'addOutput'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'Left Of Selected',
-          enabled: hasSelection,
-          action: 'addClauseLeft'
-        },
-        {
-          label: 'Right Of Selected',
-          enabled: hasSelection,
-          action: 'addClauseRight'
-        }]
-      }, {
-        label: 'Remove Clause',
-        enabled: hasSelection,
-        action: 'removeClause'
-      }
-    ], [
-      {
         label: 'Select Cell Above',
         accelerator: 'Shift + Enter',
         enabled: hasSelection,
