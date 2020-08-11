3f6c7fdd9e2e079be36748cb44ac324d1c94cdf4
Philipp Fromme <philippfromme@outlook.com>
2020-08-11 22:08:19 +0200

    feat(client): return editor action return value

--- client/src/app/tabs/bpmn/BpmnEditor.js
+++ client/src/app/tabs/bpmn/BpmnEditor.js
@@ -606,7 +606,7 @@ export class BpmnEditor extends CachedComponent {
     }
 
     // TODO(nikku): handle all editor actions
-    modeler.get('editorActions').trigger(action, context);
+    return modeler.get('editorActions').trigger(action, context);
   }
 
   handleSetColor = (fill, stroke) => {
