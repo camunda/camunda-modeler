ec36207b6666a24f93e5e72f920ef7e9e4406611
wederbn <benjamin.weder@iaas.uni-stuttgart.de>
2020-08-27 17:55:38 +0200

    Add elements of replacement fragment to diagram

--- resources/plugins/QuantME-CamundaPlugin/replacement/QuantMETransformator.js
+++ resources/plugins/QuantME-CamundaPlugin/replacement/QuantMETransformator.js
@@ -22,6 +22,10 @@ export default class QuantMETransformator {
     // register the startReplacementProcess() function as editor action to enable the invocation from the menu
     const editorActions = injector.get('editorActions', false);
 
+    console.log('Modeling: ' + _getMethods(modeling));
+    console.log('bpmnjs: ' + _getMethods(bpmnjs));
+    console.log('elementRegistry: ' + _getMethods(elementRegistry));
+
     // update locally stored QRMs if update is received
     eventBus.on('QRMs.updated', 1000, (event) => {
       console.log('Received event to update QRMs!');
@@ -114,9 +118,98 @@ export default class QuantMETransformator {
         return false;
       }
 
-      // TODO: replace task with QRM
       console.log('Replacement element: ', replacementElement);
-      return false;
+      return insertShape(parent, replacementElement, {})['success'];
+    }
+
+    /**
+     * TODO
+     *
+     * @param parent
+     * @param replacement
+     * @param idMap
+     * @return
+     */
+    function insertShape(parent, newElement, idMap) {
+      console.log('Inserting shape for element: ', newElement);
+
+      // create new id map if not provided
+      if (idMap === undefined) {
+        idMap = {};
+      }
+
+      let element;
+      if (newElement.$type !== 'bpmn:SequenceFlow') {
+        // create new shape for this element
+        element = modeling.createShape({ type: newElement.$type }, { x: 50, y: 50 }, parent, {});
+      } else {
+        // create connection between two previously created elements
+        let sourceElement = elementRegistry.get(idMap[newElement.sourceRef.id]);
+        let targetElement = elementRegistry.get(idMap[newElement.targetRef.id]);
+        element = modeling.connect(sourceElement, targetElement, { type: 'bpmn:SequenceFlow' });
+      }
+
+      // retrieve properties that have to be copied from the replacement fragment to the new element
+      let properties = {};
+      for (let key in newElement) {
+
+        // ignore properties from parent element
+        if (!newElement.hasOwnProperty(key)) {
+          continue;
+        }
+
+        // ignore properties such as type
+        if (key.startsWith('$')) {
+          continue;
+        }
+
+        // ignore id as it is automatically generated with the shape
+        if (key === 'id') {
+          // store id to create sequence flows
+          idMap[newElement['id']] = element.id;
+          continue;
+        }
+
+        // ignore flow elements, as the children are added afterwards
+        if (key === 'flowElements') {
+          continue;
+        }
+
+        properties[key] = newElement[key];
+      }
+
+      // update the properties of the new element
+      modeling.updateProperties(element, properties);
+
+      // recursively handle children of the current element
+      let success = true;
+      let flowElements = newElement.flowElements;
+      let sequenceFlows = [];
+      if (flowElements) {
+        console.log('Element contains children. Adding corresponding shapes...');
+        for (let i = 0; i < flowElements.length; i++) {
+
+          // skip sequence flows and add after all other elements
+          if (flowElements[i].$type === 'bpmn:SequenceFlow') {
+            sequenceFlows.push(flowElements[i]);
+            continue;
+          }
+
+          let result = insertShape(element, flowElements[i], idMap);
+          success = success && result['success'];
+          idMap = result['idMap'];
+        }
+
+        // handle sequence flow with new ids of added elements
+        for (let i = 0; i < sequenceFlows.length; i++) {
+          let result = insertShape(element, sequenceFlows[i], idMap);
+          success = success && result['success'];
+          idMap = result['idMap'];
+        }
+      }
+
+      // return success flag and idMap with id mappings of this element and all children
+      return { success: success, idMap: idMap };
     }
 
     /**
@@ -156,4 +249,14 @@ export async function isReplaceable(element) {
   // no suited QRM found, and therefore, no replacement possible
   console.log('No suited QRM found for task: ', element);
   return true; // FIXME: QRMs are currently not accessible from this method
+}
+
+// TODO: delete
+function _getMethods(obj) {
+  let properties = new Set();
+  let currentObj = obj;
+  do {
+    Object.getOwnPropertyNames(currentObj).map(item => properties.add(item));
+  } while ((currentObj = Object.getPrototypeOf(currentObj)));
+  return [...properties.keys()].filter(item => typeof obj[item] === 'function');
 }
