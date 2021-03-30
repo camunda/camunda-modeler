794c88235717365156de9c4a50684bc64db8cdaa
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2021-03-30 15:16:49 +0200

    style: fix lint errors

--- client/test/mocks/form-js/index.js
+++ client/test/mocks/form-js/index.js
@@ -1,3 +1,13 @@
+/**
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.
+ *
+ * Camunda licenses this file to you under the MIT; you may not use this file
+ * except in compliance with the MIT License.
+ */
+
 export function createFormEditor({ schema }) {
   if (schema.importError) {
     throw new Error('error');
@@ -13,7 +23,7 @@ export function createFormEditor({ schema }) {
       if (!listeners[ event ]) {
         listeners[ event ] = [];
       }
-  
+
       listeners[ event ].push(callback);
     },
     off() {},
@@ -22,5 +32,5 @@ export function createFormEditor({ schema }) {
         listeners[ event ].forEach(callback => callback());
       }
     }
-  }
+  };
 }
