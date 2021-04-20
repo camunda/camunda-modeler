c96fb5b60450c85006d7fb632c8680eba9a76964
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2021-04-20 16:16:46 +0200

    chore: move backend component entirely to preload script

---
+++ app/lib/util/generate-id.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.
+ *
+ * Camunda licenses this file to you under the MIT; you may not use this file
+ * except in compliance with the MIT License.
+ */
+
+const Ids = require('ids');
+
+const ids = new Ids([ 32, 36, 1 ]);
+
+module.exports = function generateId() {
+  return ids.next();
+};
