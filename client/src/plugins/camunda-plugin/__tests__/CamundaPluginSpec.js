fc89f8eb7edc73f300a4707d28093390ba749fc9
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2021-05-10 15:18:55 +0200

    fix(camunda-plugin): expose getVersion to start instance plugin
    Closes #2249

---
+++ client/src/plugins/camunda-plugin/__tests__/CamundaPluginSpec.js
@@ -0,0 +1,62 @@
+/**
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.
+ *
+ * Camunda licenses this file to you under the MIT; you may not use this file
+ * except in compliance with the MIT License.
+ */
+
+/* global sinon */
+
+import React from 'react';
+
+import { shallow } from 'enzyme';
+
+import CamundaPlugin from '..';
+
+describe('<CamundaPlugin>', () => {
+
+  let fetch;
+
+  beforeEach(() => {
+    fetch = sinon.stub(window, 'fetch').rejects(new Error('fetch is disabled'));
+  });
+
+
+  afterEach(() => {
+    fetch.restore();
+  });
+
+
+  it('should render', () => {
+    shallow(<CamundaPlugin />);
+  });
+
+
+  it('should expose DeployService', () => {
+
+    // given
+    const tree = shallow(<CamundaPlugin />);
+    const methods = [
+      'deployWithConfiguration',
+      'getSavedDeployConfiguration',
+      'getDeployConfigurationFromUserInput',
+      'saveDeployConfiguration',
+      'canDeployWithConfiguration',
+      'getVersion'
+    ];
+
+    // when
+    const startInstance = tree.find('StartInstanceTool');
+    const deployService = startInstance.prop('deployService');
+
+    // then
+    expect(deployService).to.exist;
+
+    for (const method of methods) {
+      expect(typeof deployService[method]).to.be.eql('function');
+    }
+  });
+});
