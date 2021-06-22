38a3ba7b0ac8aea9e3ee70ee122e6f0df88f85a1
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2021-06-22 13:15:30 +0200

    docs: document Release Info
    Related to #2303

--- client/src/plugins/version-info/ReleaseInfo.js
+++ client/src/plugins/version-info/ReleaseInfo.js
@@ -11,7 +11,34 @@
 import React from 'react';
 import css from './ReleaseInfo.less';
 
-
+/**
+ * Release info notice which is displayed once user clicks the version number on the status bar.
+ *
+ * Custom-styled HTML tags which you can use in the notice include:
+ *
+ *   * `h1-4`
+ *   * `a`
+ *   * `p`
+ *   * `ul`
+ *   * `ol`
+ *
+ * Notice that the text content of the `<a href>` is used as a label for usage tracking.
+ * Therefore, it's essential to use [clear link wording](https://developer.mozilla.org/en-US/docs/Learn/HTML/Introduction_to_HTML/Creating_hyperlinks#use_clear_link_wording).
+ *
+ * @example
+ *
+ * ```jsx
+ * <div className={ css.ReleaseInfo }>
+ *   <h1>New Form Inputs</h1>
+ *   <p>This release adds support for the following input fields:</p>
+ *   <ul>
+ *     <li>Number</li>
+ *     <li>Password</li>
+ *   </ul>
+ *   <p>To learn more, read <a href="...">Camunda Forms documentation</a></p>
+ * </div>
+ * ```
+ */
 export function ReleaseInfo(props) {
   return (
     <div className={ css.ReleaseInfo }>
