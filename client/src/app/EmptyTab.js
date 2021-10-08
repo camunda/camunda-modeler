ce6363175f4ffde3e41aeef38f4aac636eb4c66e
Beatriz Mendes <bea.smendes98@gmail.com>
2021-10-08 16:09:10 +0200

    feat: update welcome tab UI
    Closes #2479

--- client/src/app/EmptyTab.js
+++ client/src/app/EmptyTab.js
@@ -10,6 +10,12 @@
 
 import React, { PureComponent } from 'react';
 
+import CloudIcon from '../../resources/icons/Platform.svg';
+import PlatformIcon from '../../resources/icons/Cloud.svg';
+import BPMNIcon from '../../resources/icons/file-types/BPMN.svg';
+import DMNIcon from '../../resources/icons/file-types/DMN.svg';
+import FormIcon from '../../resources/icons/file-types/Form.svg';
+
 import css from './EmptyTab.less';
 
 import {
@@ -27,36 +33,76 @@ export default class EmptyTab extends PureComponent {
 
   triggerAction() {}
 
-  render() {
+  renderDiagramButton = (action, title, icon) => {
 
     const {
       onAction
     } = this.props;
 
     return (
+      <button className="btn btn-secondary" onClick={ () => onAction(action) }>
+        {icon}
+        {title}
+      </button>
+    );
+  }
+
+  renderCloudColumn = () => {
+
+    return (
+      <div className="create-buttons">
+
+        <PlatformIcon className="engineIcon" />
+
+        <span className="engine">Camunda Cloud</span>
+        <p>Create a new file</p>
+
+        {this.renderDiagramButton('create-cloud-bpmn-diagram', 'BPMN diagram', <BPMNIcon />)}
+        {
+          !Flags.get(DISABLE_FORM) && (
+            this.renderDiagramButton('create-form', 'Form', <FormIcon />)
+          )
+        }
+      </div>
+    );
+  }
+
+  renderPlatformColumn = () => {
+
+    return (
+      <div className="create-buttons">
+
+        <CloudIcon className="engineIcon" />
+
+        <span className="engine">Camunda Platform</span>
+        <p>Create a new file</p>
+
+        {this.renderDiagramButton('create-bpmn-diagram', 'BPMN diagram', <BPMNIcon />)}
+        {
+          !Flags.get(DISABLE_DMN) && (
+            this.renderDiagramButton('create-dmn-diagram', 'DMN diagram', <DMNIcon />)
+          )
+        }
+        {
+          !Flags.get(DISABLE_FORM) && (
+            this.renderDiagramButton('create-form', 'Form', <FormIcon />)
+          )
+        }
+      </div>
+    );
+  }
+
+  render() {
+
+    return (
       <Tab className={ css.EmptyTab }>
-        <div className="create-buttons">
-          <p>Create a new file:</p>
-          <button className="btn btn-secondary" onClick={ () => onAction('create-bpmn-diagram') }>BPMN diagram (Camunda Platform)</button>
+        {this.renderPlatformColumn()}
 
-          {
-            !Flags.get(DISABLE_ZEEBE) && (
-              <button className="btn btn-secondary" onClick={ () => onAction('create-cloud-bpmn-diagram') }>BPMN diagram (Camunda Cloud)</button>
-            )
-          }
-
-          {
-            !Flags.get(DISABLE_DMN) && (
-              <button className="btn btn-secondary" onClick={ () => onAction('create-dmn-diagram') }>DMN diagram (Camunda Platform)</button>
-            )
-          }
-
-          {
-            !Flags.get(DISABLE_FORM) && (
-              <button className="btn btn-secondary" onClick={ () => onAction('create-form') }>Form (Camunda Platform or Cloud)</button>
-            )
-          }
-        </div>
+        {
+          !Flags.get(DISABLE_ZEEBE) && (
+            this.renderCloudColumn()
+          )
+        }
       </Tab>
     );
   }
