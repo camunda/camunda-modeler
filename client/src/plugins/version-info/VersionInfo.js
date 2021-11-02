fc8520867de5f0bb7c7111aa66c8888b1260ac1a
Niklas Kiefer <niklas.kiefer@camunda.com>
2021-11-02 09:34:40 +0100

    style: update color usage with new scheme

--- client/src/plugins/version-info/VersionInfo.js
+++ client/src/plugins/version-info/VersionInfo.js
@@ -135,7 +135,7 @@ function UnreadMarker(props) {
       aria-label="unread" role="img" xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 100 100"
     >
-      <circle fill="currentColor" cx="50" cy="50" r="50" />
+      <circle cx="50" cy="50" r="50" />
     </svg>
   </span>
   );
