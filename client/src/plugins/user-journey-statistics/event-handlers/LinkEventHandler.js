82ea95b399c3bd2c7c1c8e30f6343b412220132c
Beatriz Mendes <bea.smendes98@gmail.com>
2022-05-31 16:51:23 +0200

    feat(mixpanel): track link and button clicks
    Related to #2934

---
+++ client/src/plugins/user-journey-statistics/event-handlers/LinkEventHandler.js
@@ -0,0 +1,59 @@
+/**
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.
+ *
+ * Camunda licenses this file to you under the MIT; you may not use this file
+ * except in compliance with the MIT License.
+ */
+
+const TRACKED_LINK_PARENT_IDS = [ 'version-info-overlay', 'welcome-page-cloud', 'welcome-page-platform', 'welcome-page-learn-more' ];
+
+export default class LinkEventHandler {
+  constructor(props) {
+    this.track = props.track;
+
+    document.addEventListener('click', this.trackClick);
+  }
+
+   trackClick = ({ target }) => {
+     let payload, payloadType;
+
+     for (const parent of TRACKED_LINK_PARENT_IDS) {
+
+       if (target.matches(`#${parent} a[href]`)) {
+         payload = {
+           parent,
+           label: target.textContent
+         };
+         payloadType = 'link';
+
+         // Only provide href for external links
+         if (target.href.startsWith('http') && !target.href.startsWith('http://localhost')) {
+           payload.link = target.href;
+           payload.type = 'external-link';
+         } else {
+           payload.type = 'internal-link';
+         }
+       }
+
+       if (target.matches(`#${parent} button`)) {
+         payloadType = 'button';
+         payload = {
+           parent,
+           label: target.textContent,
+           type: payloadType
+         };
+       }
+     }
+
+     if (payloadType) {
+       const action = payloadType === 'link' ? 'opened' : 'clicked';
+       const event = `${payloadType}:${action}`;
+
+       this.track(event, payload);
+     }
+
+   }
+}
---
+++ client/src/plugins/user-journey-statistics/event-handlers/__tests__/LinkEventHandlerSpec.js
@@ -0,0 +1,142 @@
+/**
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.
+ *
+ * Camunda licenses this file to you under the MIT; you may not use this file
+ * except in compliance with the MIT License.
+ */
+
+/* global sinon */
+
+import MochaTestContainer from 'mocha-test-container-support';
+import MixpanelHandler from '../../MixpanelHandler';
+
+import LinkEventHandler from '../LinkEventHandler';
+
+
+describe('<LinkEventHandler>', () => {
+
+  let container, linkInside, linkOutside, linkHttp, button, buttonOutside, track;
+
+  beforeEach(function() {
+    container = MochaTestContainer.get(this);
+
+    const versionInfoOverlay = document.createElement('div');
+    versionInfoOverlay.id = 'version-info-overlay';
+
+    linkInside = createAnchor('inside');
+    linkOutside = createAnchor('outside');
+
+    linkHttp = createAnchor('http', 'https://camunda.com/');
+    button = createButton('click-me');
+    buttonOutside = createButton('click-outside');
+
+    versionInfoOverlay.appendChild(linkInside);
+    versionInfoOverlay.appendChild(linkHttp);
+    versionInfoOverlay.appendChild(button);
+
+    container.appendChild(linkOutside);
+    container.appendChild(versionInfoOverlay);
+
+    MixpanelHandler.getInstance().enable('token', 'id', 'stage');
+
+    track = sinon.spy();
+    new LinkEventHandler({ track });
+  });
+
+
+  afterEach(function() {
+    container.innerHTML = '';
+  });
+
+
+  describe('should send', () => {
+
+    it('internal link', () => {
+
+      // when
+      linkInside.click();
+
+      // then
+      expect(track).to.have.been.calledOnceWith('link:opened', {
+        type: 'internal-link',
+        label: 'inside',
+        parent: 'version-info-overlay'
+      });
+    });
+
+
+    it('external link', () => {
+
+      // when
+      linkHttp.click();
+
+      // then
+      expect(track).to.have.been.calledOnceWith('link:opened', {
+        type: 'external-link',
+        label: 'http',
+        link: 'https://camunda.com/',
+        parent: 'version-info-overlay'
+      });
+    });
+
+
+    it('button', () => {
+
+      // when
+      button.click();
+
+      // then
+      expect(track).to.have.been.calledOnceWith('button:clicked', {
+        type: 'button',
+        label: 'click-me',
+        parent: 'version-info-overlay'
+      });
+    });
+
+  });
+
+
+  describe('should not send if out of scope', () => {
+
+    it('link', () => {
+
+      // when
+      linkOutside.click();
+
+      // then
+      expect(track).not.to.have.been.called;
+    });
+
+
+    it('button', () => {
+
+      // when
+      buttonOutside.click();
+
+      // then
+      expect(track).not.to.have.been.called;
+    });
+  });
+
+});
+
+
+function createAnchor(label, href = '#') {
+  const anchor = document.createElement('a');
+  anchor.textContent = label;
+  anchor.href = href;
+
+  // prevent redirect
+  anchor.addEventListener('click', event => event.preventDefault());
+
+  return anchor;
+}
+
+function createButton(label) {
+  const button = document.createElement('button');
+  button.textContent = label;
+  return button;
+}
