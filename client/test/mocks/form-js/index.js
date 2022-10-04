ff04712238538b7404a0489abd180e1dfc528c05
Niklas Kiefer <niklas.kiefer@camunda.com>
2022-10-04 14:39:28 +0200

    feat(client/form): replace form editor with playground
    Closes #3126

--- client/test/mocks/form-js/index.js
+++ client/test/mocks/form-js/index.js
@@ -101,3 +101,81 @@ export class FormEditor {
     return this.schema;
   }
 }
+
+export class FormPlayground {
+  constructor(options = {}) {
+    this.options = options;
+
+    this._editor = new FormEditor(options);
+
+    this._emitLayoutChanged = options.emitLayoutChanged;
+
+    this.schema = null;
+
+    this.listeners = {};
+  }
+
+  attachTo() {}
+
+  detach() {}
+
+  on(event, priority, callback) {
+    if (!callback) {
+      callback = priority;
+    }
+
+    if (!this.listeners[ event ]) {
+      this.listeners[ event ] = [];
+    }
+
+    this.listeners[ event ].push(callback);
+  }
+
+  off(event, callback) {
+    if (this.listeners[ event ]) {
+      this.listeners[ event ] = this.listeners[ event ].filter(cb => cb !== callback);
+    }
+  }
+
+  emit(event, context) {
+    if (this.listeners[ event ]) {
+      this.listeners[ event ].forEach(callback => callback(context));
+    }
+  }
+
+  get(moduleName) {
+    return this._editor.get(moduleName);
+  }
+
+  getEditor() {
+    return this._editor;
+  }
+
+  getSchema() {
+    return this.saveSchema();
+  }
+
+  saveSchema() {
+    return this._editor.schema;
+  }
+
+  open() {
+    this._emitLayoutChanged && this.emit('formPlayground.layoutChanged', {
+      layout: {
+        'form-preview': { open: true },
+        'form-input': { open: true },
+        'form-output': { open: true }
+      }
+    });
+  }
+
+  collapse() {
+    this._emitLayoutChanged && this.emit('formPlayground.layoutChanged', {
+      layout: {
+        'form-preview': { open: false },
+        'form-input': { open: false },
+        'form-output': { open: false }
+      }
+    });
+  }
+}
