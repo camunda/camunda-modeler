342c071cd9e4ec39972b6eb80bb6bb572c600d8d
Philipp <philippfromme@outlook.com>
2022-11-02 09:23:49 +0100

    feat: show errors and warnings in problems panel
    Closes #3200

--- client/src/app/tabs/panel/tabs/__tests__/LintingTabSpec.js
+++ client/src/app/tabs/panel/tabs/__tests__/LintingTabSpec.js
@@ -34,18 +34,18 @@ describe('<LintingTab>', function() {
     expect(wrapper.find('.panel__link')).to.have.length(1);
     expect(wrapper.find('.panel__link--active')).to.have.length(1);
 
-    expect(wrapper.find('.panel__link').at(0).find('.panel__link-label').text()).to.equal('Errors');
+    expect(wrapper.find('.panel__link').at(0).find('.panel__link-label').text()).to.equal('Problems');
     expect(wrapper.find('.panel__link').at(0).hasClass('panel__link--active')).to.be.true;
 
     expect(wrapper.find('.panel__link-number')).to.have.length(1);
     expect(wrapper.find('.panel__link-number').text()).to.equal('1');
 
-    expect(wrapper.find('.linting-issue__text')).to.have.length(1);
-    expect(wrapper.find('.linting-issue__text').text()).to.equal('Error : foo - Form field of type <number> not supported by Camunda Cloud 1.0');
+    expect(wrapper.find('.linting-issue__content')).to.have.length(1);
+    expect(wrapper.find('.linting-issue__content').text()).to.equal('Foo message');
   });
 
 
-  it('should render (no errors)', function() {
+  it('should render (no problems)', function() {
 
     // when
     const wrapper = renderLintingTab({
@@ -56,14 +56,14 @@ describe('<LintingTab>', function() {
     expect(wrapper.find('.panel__link')).to.have.length(1);
     expect(wrapper.find('.panel__link--active')).to.have.length(1);
 
-    expect(wrapper.find('.panel__link').at(0).find('.panel__link-label').text()).to.equal('Errors');
+    expect(wrapper.find('.panel__link').at(0).find('.panel__link-label').text()).to.equal('Problems');
     expect(wrapper.find('.panel__link').at(0).hasClass('panel__link--active')).to.be.true;
 
     expect(wrapper.find('.panel__link-number')).to.have.length(1);
     expect(wrapper.find('.panel__link-number').text()).to.equal('0');
 
-    expect(wrapper.find('.linting-issue')).to.have.length(1);
-    expect(wrapper.find('.linting-issue').text()).to.equal('No errors.');
+    expect(wrapper.find('.linting-issue--empty')).to.have.length(1);
+    expect(wrapper.find('.linting-issue--empty').text()).to.equal('No problems found.');
   });
 
 
@@ -76,45 +76,40 @@ describe('<LintingTab>', function() {
           category: 'error',
           id: 'foo',
           name: 'Foo',
-          path: [],
           message: 'foo error'
         },
         {
           category: 'error',
           id: 'bar',
           name: 'Bar 1',
-          path: [],
           message: 'bar 1 error'
         },
         {
           category: 'error',
           id: 'bar',
           name: 'Bar 2',
-          path: [],
           message: 'bar 2 error'
         },
         {
-          category: 'error',
+          category: 'warn',
           id: 'baz',
-          path: [],
-          message: 'baz 1 error'
+          message: 'baz 1 warning'
         },
         {
           category: 'error',
           id: 'baz',
-          path: [],
           message: 'baz 2 error'
         }
       ]
     });
 
     // then
-    expect(wrapper.find('.linting-issue__text')).to.have.length(5);
-    expect(wrapper.find('.linting-issue__text').at(0).text()).to.equal('Error : Bar 1 - bar 1 error');
-    expect(wrapper.find('.linting-issue__text').at(1).text()).to.equal('Error : Bar 2 - bar 2 error');
-    expect(wrapper.find('.linting-issue__text').at(2).text()).to.equal('Error : baz - baz 1 error');
-    expect(wrapper.find('.linting-issue__text').at(3).text()).to.equal('Error : baz - baz 2 error');
-    expect(wrapper.find('.linting-issue__text').at(4).text()).to.equal('Error : Foo - foo error');
+    expect(wrapper.find('.linting-issue__content')).to.have.length(5);
+    expect(wrapper.find('.linting-issue__content').at(0).text()).to.equal('bar 1 error');
+    expect(wrapper.find('.linting-issue__content').at(1).text()).to.equal('bar 2 error');
+    expect(wrapper.find('.linting-issue__content').at(2).text()).to.equal('baz 1 warning');
+    expect(wrapper.find('.linting-issue__content').at(3).text()).to.equal('baz 2 error');
+    expect(wrapper.find('.linting-issue__content').at(4).text()).to.equal('foo error');
   });
 
 
@@ -128,14 +123,14 @@ describe('<LintingTab>', function() {
     });
 
     // then
-    wrapper.find('.linting-issue__link').at(0).simulate('click');
+    wrapper.find('.linting-issue').at(0).simulate('click');
 
     // then
     expect(onActionSpy).to.have.been.calledOnce;
     expect(onActionSpy).to.have.been.calledWithMatch('showLintError', {
       id: 'foo',
-      path: [],
-      message: 'Form field of type <number> not supported by Camunda Cloud 1.0'
+      name: 'Foo',
+      message: 'Foo message'
     });
   });
 
@@ -155,8 +150,8 @@ const defaultLinting = [
   {
     category: 'error',
     id: 'foo',
-    path: [],
-    message: 'Form field of type <number> not supported by Camunda Cloud 1.0'
+    name: 'Foo',
+    message: 'Foo message'
   }
 ];
 
