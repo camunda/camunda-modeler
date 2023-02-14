e401bbd41c2fde720ad3e0ef1ea599402131b6e5
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2023-02-14 19:37:25 +0100

    chore(update-checks): remove `:` from title

--- client/src/plugins/update-checks/constants.js
+++ client/src/plugins/update-checks/constants.js
@@ -14,7 +14,7 @@ export const BUTTON_NEGATIVE = 'Skip this version';
 
 export const BUTTON_POSITIVE = 'Go to download page';
 
-export const RELEASE_NOTES_TITLE = 'Release notes:';
+export const RELEASE_NOTES_TITLE = 'Release notes';
 
 export const INFO_TEXT = 'Camunda Modeler @@1 is available. Your version is @@2.';
 
