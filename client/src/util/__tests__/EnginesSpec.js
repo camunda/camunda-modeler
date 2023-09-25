7297f1607d2d1ed6fad4aa24c38fb56b17f31c96
Martin Stamm <martin.stamm@camunda.com>
2023-09-25 11:15:38 +0200

    chore(Engines): mark 8.3 and 7.20 as stable

--- client/src/util/__tests__/EnginesSpec.js
+++ client/src/util/__tests__/EnginesSpec.js
@@ -27,9 +27,9 @@ describe('util/Engines', function() {
       };
     }
 
-    it('Platform', verifyLatestStable(ENGINES.PLATFORM, '7.19.0'));
+    it('Platform', verifyLatestStable(ENGINES.PLATFORM, '7.20.0'));
 
-    it('Cloud', verifyLatestStable(ENGINES.CLOUD, '8.2.0'));
+    it('Cloud', verifyLatestStable(ENGINES.CLOUD, '8.3.0'));
 
   });
 
