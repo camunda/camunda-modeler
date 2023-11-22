fede44d842418a8ad114f5949b42b3d2c69fa294
Philipp <philippfromme@outlook.com>
2023-11-22 17:02:08 +0100

    chore: adjust label
    Related to https://github.com/bpmn-io/dmn-js/issues/801

--- client/src/app/tabs/dmn/DmnEditor.js
+++ client/src/app/tabs/dmn/DmnEditor.js
@@ -907,7 +907,7 @@ export class DmnEditor extends CachedComponent {
           !isDrd && (
             <div className="top">
               <button id="button-edit-drd" className="button" onClick={ this.handleEditDrdClick }>Edit DRD</button>
-              <button id="button-toggle-overview" className="button" onClick={ this.toggleOverview }>{ overviewOpen ? 'Close' : 'Open' } Overview</button>
+              <button id="button-toggle-overview" className="button" onClick={ this.toggleOverview }>{ overviewOpen ? 'Close' : 'Open' } overview</button>
             </div>
           )
         }
