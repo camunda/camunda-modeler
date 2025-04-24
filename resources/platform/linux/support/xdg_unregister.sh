74fa2c57279b992549f76b1aa51457457efd91fa
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2025-04-24 17:03:13 +0200

    feat(platform/linux): extract desktop file from register script
    * make desktop file external resource
    * use on board utilities to modify and validate it

--- resources/platform/linux/support/xdg_unregister.sh
+++ resources/platform/linux/support/xdg_unregister.sh
@@ -12,11 +12,14 @@ done
 WORKING_DIR=`dirname "$PRG"`
 
 #!/bin/bash
-echo "Uninstalling icons and desktop entry..."
+echo "Unregistering mime types..."
+xdg-mime uninstall $WORKING_DIR/mime-types.xml
+
+echo "Uninstalling desktop file..."
 xdg-desktop-menu uninstall camunda-modeler.desktop
+
+echo "Uninstalling icons..."
 xdg-icon-resource uninstall --size  16 camunda-modeler
 xdg-icon-resource uninstall --size  48 camunda-modeler
 xdg-icon-resource uninstall --size 128 camunda-modeler
-echo "Unregistering mime types..."
-xdg-mime uninstall $WORKING_DIR/mime-types.xml
 echo "done."
