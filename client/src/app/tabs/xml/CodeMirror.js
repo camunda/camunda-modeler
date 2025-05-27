9a88da7f15d52a68175e95aeff12ac8c93c6a510
Philipp Fromme <philipp.fromme@camunda.com>
2025-05-27 16:37:13 +0200

    feat: add Codemirror theme

--- client/src/app/tabs/xml/CodeMirror.js
+++ client/src/app/tabs/xml/CodeMirror.js
@@ -16,6 +16,8 @@ import { EditorView } from '@codemirror/view';
 
 import { EditorState, Compartment } from '@codemirror/state';
 
+import { vscodeLight } from '@uiw/codemirror-theme-vscode';
+
 import {
   undo,
   redo,
@@ -60,6 +62,12 @@ export default function create() {
           'tabindex': 0
         }),
         EditorView.lineWrapping,
+        vscodeLight,
+        EditorView.theme({
+          '.cm-content': {
+            fontFamily: 'Consolas, "Courier New", monospace'
+          }
+        }),
         ...extensions
       ]
     });
