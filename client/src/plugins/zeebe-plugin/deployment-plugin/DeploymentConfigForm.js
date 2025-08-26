a679a59f6c22444c6d64b258f80eb336b70ebc39
Simon Steinrücken <simon.steinruecken@camunda.com>
2025-08-26 11:24:33 +0200

    wip selector

--- client/src/plugins/zeebe-plugin/deployment-plugin/DeploymentConfigForm.js
+++ client/src/plugins/zeebe-plugin/deployment-plugin/DeploymentConfigForm.js
@@ -19,6 +19,7 @@ import {
 import {
   Radio,
   Section,
+  Select,
   TextInput,
   ToggleSwitch
 } from '../../../shared/ui';
@@ -59,9 +60,11 @@ export default function DeploymentConfigForm(props) {
     renderHeader = null,
     renderSubmit = 'Submit',
     validateForm,
-    validateField
+    validateField,
+    connections
   } = props;
 
+  console.log(connections);
   const getFieldError = (meta, fieldName) => {
     return _getFieldError(fieldName) || (meta.touched && meta.error);
   };
@@ -82,212 +85,31 @@ export default function DeploymentConfigForm(props) {
                 {
                   renderHeader && (
                     <Section.Header className="form-header">
-                      { renderHeader }
+                      Select Connection
                     </Section.Header>
                   )
                 }
-                {
-                  renderDescription && (
-                    <Section.Body className="form-description">
-                      { renderDescription }
-                    </Section.Body>
-                  )
-                }
-                <Section.Body className="form-body">
-                  <fieldset className="fieldset">
-                    <div className="fields">
-                      <Field
-                        name="endpoint.targetType"
-                        component={ Radio }
-                        label={ 'Target' }
-                        className="radio-vertical"
-                        values={
-                          [
-                            { value: TARGET_TYPES.CAMUNDA_CLOUD, label: LABELS.CAMUNDA_CLOUD },
-                            { value: TARGET_TYPES.SELF_HOSTED, label: LABELS.SELF_HOSTED }
-                          ]
-                        }
-                      />
-                      {
-                        props.values.endpoint.targetType === TARGET_TYPES.SELF_HOSTED && (
-                          <React.Fragment>
-                            <Field
-                              name="endpoint.contactPoint"
-                              component={ TextInput }
-                              label={ LABELS.CONTACT_POINT }
-                              validate={ value => validateField('endpoint.contactPoint', value) }
-                              fieldError={ getFieldError }
-                              hint={ HINTS.CONTACT_POINT }
-                              autoFocus
-                            />
-                            {
-                              props.values.endpoint.targetType === TARGET_TYPES.SELF_HOSTED
-                                && (
-                                  <Field
-                                    name="deployment.tenantId"
-                                    component={ TextInput }
-                                    label={ LABELS.TENANT_ID }
-                                    hint="Optional"
-                                  />
-                                )
-                            }
-                            <Field
-                              name="endpoint.authType"
-                              component={ Radio }
-                              label={ 'Authentication' }
-                              className="radio-horizontal"
-                              values={
-                                [
-                                  { value: AUTH_TYPES.NONE, label: LABELS.AUTH_TYPE_NONE },
-                                  { value: AUTH_TYPES.BASIC, label: LABELS.AUTH_TYPE_BASIC_AUTH },
-                                  { value: 'COOKIE' , label: 'Cookie' },
-                                  { value: AUTH_TYPES.OAUTH, label: LABELS.AUTH_TYPE_OAUTH }
-                                ]
-                              }
-                            />
-                          </React.Fragment>
-                        )
-                      }
-                      {
-                        props.values.endpoint.targetType === TARGET_TYPES.SELF_HOSTED
-                          && props.values.endpoint.authType === AUTH_TYPES.BASIC
-                          && (
-                            <React.Fragment>
-                              <Field
-                                name="endpoint.basicAuthUsername"
-                                component={ TextInput }
-                                label={ LABELS.BASIC_AUTH_USERNAME }
-                                fieldError={ getFieldError }
-                                validate={ value => validateField('endpoint.basicAuthUsername', value) }
-                              />
-                              <Field
-                                name="endpoint.basicAuthPassword"
-                                component={ TextInput }
-                                label={ LABELS.BASIC_AUTH_PASSWORD }
-                                fieldError={ getFieldError }
-                                validate={ value => validateField('endpoint.basicAuthPassword', value) }
-                                type="password"
-                              />
-                            </React.Fragment>
-                          )
-                      }
-                      {
-                        props.values.endpoint.targetType === TARGET_TYPES.SELF_HOSTED
-                          && props.values.endpoint.authType === 'COOKIE'
-                          && (
-                            <React.Fragment>
-                              <Field
-                                name="endpoint.cookieURL"
-                                component={ TextInput }
-                                label={ 'Login URL' }
-                                fieldError={ getFieldError }
-                                validate={ value => validateField('endpoint.cookieURL', value) }
-                              />
-                              <Field
-                                name="endpoint.basicAuthUsername"
-                                component={ TextInput }
-                                label={ LABELS.BASIC_AUTH_USERNAME }
-                                fieldError={ getFieldError }
-                                validate={ value => validateField('endpoint.basicAuthUsername', value) }
-                              />
-                              <Field
-                                name="endpoint.basicAuthPassword"
-                                component={ TextInput }
-                                label={ LABELS.BASIC_AUTH_PASSWORD }
-                                fieldError={ getFieldError }
-                                validate={ value => validateField('endpoint.basicAuthPassword', value) }
-                                type="password"
-                              />
-                            </React.Fragment>
-                          )
-                      }
-                      {
-                        props.values.endpoint.targetType === TARGET_TYPES.SELF_HOSTED
-                          && props.values.endpoint.authType === AUTH_TYPES.OAUTH
-                          && (
-                            <React.Fragment>
-                              <Field
-                                name="endpoint.clientId"
-                                component={ TextInput }
-                                label={ LABELS.CLIENT_ID }
-                                fieldError={ getFieldError }
-                                validate={ value => validateField('endpoint.clientId', value) }
-                              />
-                              <Field
-                                name="endpoint.clientSecret"
-                                component={ TextInput }
-                                label={ LABELS.CLIENT_SECRET }
-                                fieldError={ getFieldError }
-                                validate={ value => validateField('endpoint.clientSecret', value) }
-                                type="password"
-                              />
-                              <Field
-                                name="endpoint.oauthURL"
-                                component={ TextInput }
-                                label={ LABELS.OAUTH_URL }
-                                fieldError={ getFieldError }
-                                validate={ value => validateField('endpoint.oauthURL', value) }
-                              />
-                              <Field
-                                name="endpoint.audience"
-                                component={ TextInput }
-                                label={ LABELS.OAUTH_AUDIENCE }
-                                fieldError={ getFieldError }
-                                validate={ value => validateField('endpoint.audience', value) }
-                              />
-                              <Field
-                                name="endpoint.scope"
-                                component={ TextInput }
-                                label={ LABELS.OAUTH_SCOPE }
-                              />
-                            </React.Fragment>
-                          )
-                      }
-                      {
-                        props.values.endpoint.targetType === TARGET_TYPES.CAMUNDA_CLOUD && (
-                          <React.Fragment>
-                            <Field
-                              name="endpoint.camundaCloudClusterUrl"
-                              component={ TextInput }
-                              label={ LABELS.CLUSTER_URL }
-                              fieldError={ getFieldError }
-                              validate={ value => validateField('endpoint.camundaCloudClusterUrl', value) }
-                            />
-                            <Field
-                              name="endpoint.camundaCloudClientId"
-                              component={ TextInput }
-                              label={ LABELS.CLIENT_ID }
-                              fieldError={ getFieldError }
-                              validate={ value => validateField('endpoint.camundaCloudClientId', value) }
-                            />
-                            <Field
-                              name="endpoint.camundaCloudClientSecret"
-                              component={ TextInput }
-                              label={ LABELS.CLIENT_SECRET }
-                              fieldError={ getFieldError }
-                              validate={ value => validateField('endpoint.camundaCloudClientSecret', value) }
-                              type="password"
-                            />
-                          </React.Fragment>
-                        )
-                      }
-                      <Field
-                        name={ 'endpoint.rememberCredentials' }
-                        component={ ToggleSwitch }
-                        switcherLabel={ LABELS.REMEMBER_CREDENTIALS }
-                      />
-                    </div>
-                  </fieldset>
-                  <Section.Actions>
-                    <button
-                      type="submit"
-                      className="btn btn-primary"
-                      disabled={ props.isSubmitting }
-                    >
-                      { renderSubmit }
-                    </button>
-                  </Section.Actions>
+
+
+                <Section.Body className="form-description">
+                  TODO Text: Select a connection for the current opened file.
+                  <br></br>
+                  <a href="https://docs.camunda.org/manual/latest/user-guide/tasklist/#connecting-to-a-process-engine">Manage Connections</a>
+
+
+                  <div className="form-group" style={ { marginTop:'16px' } }>
+                    <select name="connection" className="form-control">
+                      {connections.map(connection => (
+                        <option key={ connection.id } value={ connection.id }>
+                          {connection.name}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+
                 </Section.Body>
+
+
               </Section>
             </Form>
           );
