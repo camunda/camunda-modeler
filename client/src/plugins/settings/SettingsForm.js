331410d78f2b7a6e2c00683f1d443400d9b29f4f
Simon Steinrücken <simon.steinruecken@camunda.com>
2025-09-04 16:09:26 +0200

    feat: support conditional fields in settings
    Related to https://github.com/camunda/camunda-modeler/issues/5183

--- client/src/plugins/settings/SettingsForm.js
+++ client/src/plugins/settings/SettingsForm.js
@@ -10,7 +10,7 @@
 
 import React, { useEffect, useMemo } from 'react';
 
-import { Field, Form, useFormikContext } from 'formik';
+import { Field, Form, useFormikContext, getIn } from 'formik';
 
 import { map, forEach, sortBy } from 'min-dash';
 
@@ -29,19 +29,19 @@ export function SettingsForm(props) {
 
   useEffect(() => {
     dirty && submitForm();
-  }, [ formikValues ]);
+  }, [ dirty, formikValues, submitForm ]);
 
   useEffect(() => {
     forEach(values, (value, key) => {
       setFieldValue(key, value);
     });
-  }, [ values ]);
+  }, [ values, setFieldValue ]);
 
   const orderedSchema = useMemo(() => {
     if (!schema) return {};
 
     return sortSchemaByOrder(schema);
-  }, [ schema, values ]);
+  }, [ schema ]);
 
   return (<Form>
     {
@@ -70,13 +70,19 @@ function SettingsSection(props) {
 
 function SettingsField(props) {
 
-  const { type, flag } = props;
+  const { type, flag, condition, name } = props;
+
+  const { values } = useFormikContext();
 
   const flagValue = useMemo(() => {
     return Flags.get(flag);
   }, [ flag ]);
 
   const component = useMemo(() => {
+    if (condition && !isConditionMet(name, values, condition)) {
+      return null;
+    }
+
     if (type === 'text' || type === 'password') {
       return TextInput;
     }
@@ -94,13 +100,13 @@ function SettingsField(props) {
     }
 
     return null;
-  }, [ type ]);
+  }, [ condition, name, type, values ]);
 
   if (!component) {
     return null;
   }
 
-  const { name, label, description, hint, options, documentationUrl } = props;
+  const { label, description, hint, options, documentationUrl } = props;
 
   let typeProp = {};
   if (type === 'boolean') {
@@ -155,4 +161,67 @@ function sortSchemaByOrder(schema) {
   }, {});
 
   return sortedObj;
+}
+
+/**
+ * Resolves a path relative to the current path or itself if its an absolute path (contains a dot).
+ *
+ * @param {string} currentPath
+ * @param {string} targetPath - Path to resolve. If it contains a dot, it is considered an absolute path and returned as is.
+ * @returns {string} The resolved path
+ */
+export function resolvePath(currentPath, targetPath) {
+  if (targetPath.includes('.')) {
+    return targetPath;
+  }
+
+  const currentSegments = currentPath.split('.');
+
+  currentSegments.pop();
+
+  const resolvedSegments = [ ...currentSegments, targetPath ];
+  return resolvedSegments.join('.');
+}
+
+/**
+ * @typedef {Object} Condition A condition to be met for a form field to be shown
+ * @property {string} [property] - The key of the property to check, can be relative.
+ * @property {Array<Condition>} [allMatch] - A list of child conditions that must all be met.
+ * @property {any} [equals] - The value the property should be equal to.
+ * @property {Array<any>} [oneOf] - A list of possible values for the property.
+ *
+ * @example
+ * { property: 'some.key', equals: true }
+ * { property: 'some.key', oneOf: [ 'a', 'b', 'c' ] }
+ * { allMatch: [ { property: 'some.key', equals: true }, { property: 'other.key', oneOf: [ 'a', 'b' ] } ] }
+ */
+
+/**
+ * Checks if a given condition is met based on the provided form values.
+ *
+ * @param {string} propName - The base path for the property being evaluated.
+ * @param {Object} values - The object containing all form values, used to cross reference other section/fields
+ * @param {Condition} condition - The condition object to evaluate.
+ * @returns {boolean} True if the condition is met, false otherwise.
+ */
+export function isConditionMet(propName, values, condition) {
+  if (condition.allMatch) {
+    return condition.allMatch.every((childCondition) => isConditionMet(propName, values, childCondition));
+  }
+
+  if (!condition.property) {
+    return false;
+  }
+  const conditionPropPath = resolvePath(propName, condition.property);
+  const conditionPropValue = getIn(values, conditionPropPath);
+
+  if ('equals' in condition) {
+    return conditionPropValue === condition.equals;
+  }
+
+  if (condition.oneOf) {
+    return condition.oneOf.includes(conditionPropValue);
+  }
+
+  return false;
 }
