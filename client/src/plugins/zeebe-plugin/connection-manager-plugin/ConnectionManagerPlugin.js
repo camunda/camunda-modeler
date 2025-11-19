f6e29d8f67c01ceccd58fbc67e39747ca990a1eb
Simon Steinrücken <simon.steinruecken@camunda.com>
2025-11-19 15:45:52 +0100

    fix: use common connectionStatus for task testing
    Co-authored-by: Maciej Barelkowski <barmac@users.noreply.github.com>

--- client/src/plugins/zeebe-plugin/connection-manager-plugin/ConnectionManagerPlugin.js
+++ client/src/plugins/zeebe-plugin/connection-manager-plugin/ConnectionManagerPlugin.js
@@ -113,6 +113,10 @@ export default function ConnectionManagerPlugin(props) {
     })();
 
     const connectionCheckListener = (connectionCheckResult) => {
+      triggerAction('emit-event', {
+        type: 'connectionManager.connectionStatusChanged',
+        payload: connectionCheckResult
+      });
       setConnectionCheckResult(connectionCheckResult);
     };
 
