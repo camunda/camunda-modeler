1e3d4452ac0c7c554220eb0a63af29a9c956156a
Simon Steinrücken <simon.steinruecken@camunda.com>
2025-12-11 14:02:37 +0100

    fix: use tooltip instead of title for documentation link

--- client/src/shared/ui/form/DocumentationIcon.js
+++ client/src/shared/ui/form/DocumentationIcon.js
@@ -10,6 +10,8 @@
 
 import React from 'react';
 
+import { Tooltip } from '@carbon/react';
+
 import LaunchIcon from '../../../../resources/icons/Launch.svg';
 
 export default function DocumentationIcon(props) {
@@ -25,15 +27,17 @@ export default function DocumentationIcon(props) {
   }
 
   return (
-    <a
-      className="documentation-icon"
-      href={ url }
-      target="_blank" rel="noopener noreferrer"
-      onClick={ onClick }
-      title="Go to documentation"
-      { ...rest }
-    >
-      <LaunchIcon width="12" height="12" viewBox="0 0 12 12" />
-    </a>
+    <Tooltip label="Open documentation" align="bottom">
+      <a
+        className="documentation-icon"
+        href={ url }
+        target="_blank"
+        rel="noopener noreferrer"
+        onClick={ onClick }
+        { ...rest }
+      >
+        <LaunchIcon width="12" height="12" viewBox="0 0 12 12" />
+      </a>
+    </Tooltip>
   );
 }
