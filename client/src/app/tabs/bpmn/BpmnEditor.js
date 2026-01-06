e836a61ce12d05256fa827e9db2d62faac0e478d
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-01-06 08:44:27 +0100

    chore(tabs/bpmn): allow context menu on both canvas and inputs

--- client/src/app/tabs/bpmn/BpmnEditor.js
+++ client/src/app/tabs/bpmn/BpmnEditor.js
@@ -798,7 +798,7 @@ export class BpmnEditor extends CachedComponent {
     } = this.state;
 
     return (
-      <div className={ css.BpmnEditor }>
+      <div className={ css.BpmnEditor } onContextMenu={ this.handleContextMenu }>
 
         <Loader hidden={ imported && !importing } />
 
@@ -807,7 +807,6 @@ export class BpmnEditor extends CachedComponent {
             className="diagram"
             ref={ this.ref }
             onFocus={ this.handleChanged }
-            onContextMenu={ this.handleContextMenu }
           ></div>
 
           <PropertiesPanelContainer
--- client/src/app/tabs/cloud-bpmn/BpmnEditor.js
+++ client/src/app/tabs/cloud-bpmn/BpmnEditor.js
@@ -806,7 +806,7 @@ export class BpmnEditor extends CachedComponent {
     } = this.state;
 
     return (
-      <div className={ css.BpmnEditor }>
+      <div className={ css.BpmnEditor } onContextMenu={ this.handleContextMenu }>
 
         <Loader hidden={ imported && !importing } />
 
@@ -815,7 +815,6 @@ export class BpmnEditor extends CachedComponent {
             className="diagram"
             ref={ this.ref }
             onFocus={ this.handleChanged }
-            onContextMenu={ this.handleContextMenu }
           ></div>
 
           <PropertiesPanelContainer
