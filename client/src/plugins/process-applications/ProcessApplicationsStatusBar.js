8a499cb5429ddb6eee5825962b12e6ac5c3c1609
Philipp Fromme <philipp.fromme@camunda.com>
2026-01-07 16:35:45 +0100

    feat(process-applications): always show status bar item
    Closes #5344

--- client/src/plugins/process-applications/ProcessApplicationsStatusBar.js
+++ client/src/plugins/process-applications/ProcessApplicationsStatusBar.js
@@ -18,6 +18,8 @@ import { Fill } from '../../app/slot-fill';
 
 import { Overlay, Section } from '../../shared/ui';
 
+import { utmTag } from '../../util/utmTag';
+
 import ProcessApplicationIcon from '../../../resources/icons/file-types/ProcessApplication.svg';
 import ErrorIcon from '../../../resources/icons/Error.svg';
 
@@ -32,6 +34,7 @@ export default function ProcessApplicationsStatusBar(props) {
     activeTab,
     onOpen,
     onRevealInFileExplorer,
+    onCreateProcessApplication,
     processApplication,
     processApplicationItems,
     tabsProvider
@@ -43,80 +46,113 @@ export default function ProcessApplicationsStatusBar(props) {
     }
   }, [ processApplication ]);
 
-  if (!processApplication) {
-    return null;
-  }
-
-  const onClick = (path) => {
+  const onClickOpenPath = (path) => {
     onOpen(path);
 
     setIsOpen(false);
   };
 
+  const onClickCreateProcessApplication = () => {
+    onCreateProcessApplication();
+
+    setIsOpen(false);
+  };
+
   return <>
-    <Fill slot="status-bar__file" group="0_process-application">
-      <button className={ classnames('btn', css.ProcessApplicationsButton) } ref={ ref } onClick={ () => setIsOpen(!isOpen) } title="This file is part of a process application">
-        <ProcessApplicationIcon width="16" height="16" />
-      </button>
-    </Fill>
     {
-      isOpen && <Overlay className={ css.ProcessApplicationsOverlay } id="process-application-overlay" anchor={ ref.current } onClose={ () => setIsOpen(false) }>
-        <Section>
-          <Section.Header>
-            Process application
-          </Section.Header>
-          <Section.Body>
-            <ul className="files process-application-file" role="menu">
-              <li role="menuitem" className="file" key={ processApplication.file.path } title={ processApplication.file.path }>
-                <button type="button" onClick={ () => onRevealInFileExplorer(processApplication.file.path) }>
-                  <ProcessApplicationIcon className="file-icon" width="16" height="16" /><span className="file-name">{ processApplication.file.name }</span>
-                </button>
-              </li>
-            </ul>
-          </Section.Body>
-        </Section>
-        <Section>
-          <Section.Header>
-            Files
-          </Section.Header>
-          <Section.Body>
-            <ul className="files process-application-files" role="menu">
-              {
-                sortByType(processApplicationItems.filter(item => item.metadata?.type !== 'processApplication')).map(item => {
-                  const { file } = item;
-
-                  const error = hasErrorMessage(item);
-
-                  let Icon;
-
-                  if (error) {
-                    Icon = ErrorIcon;
-                  } else {
-                    const provider = tabsProvider.getProviderForFile(file);
-
-                    Icon = provider?.getIcon(file);
-                  }
-
-                  const isActiveTab = activeTab && activeTab.file.path === file.path;
-
-                  const title = error ? `${ file.path} (${ getErrorMessages(item) })` : file.path;
-
-                  return <li role="menuitem" className={
-                    classnames('file', {
-                      'file-active': isActiveTab,
-                      'file-error': error
-                    })
-                  } key={ file.path } title={ title }>
-                    <button type="button" onClick={ () => onClick(file.path) } disabled={ isActiveTab || error }>
-                      {Icon && <Icon className="file-icon" width="16" height="16" />}
-                      <span className="file-name">{ file.name }</span>
+      isProcessApplicationAllowed(activeTab) && <Fill slot="status-bar__file" group="0_process-application">
+        <button
+          className={ classnames('btn', css.ProcessApplicationsButton, { 'has-process-application': !!processApplication }) }
+          ref={ ref }
+          onClick={ () => setIsOpen(!isOpen) }
+          title={ processApplication ? 'This file is part of a process application' : 'New process application...' }
+        >
+          <ProcessApplicationIcon width="16" height="16" />
+        </button>
+      </Fill>
+    }
+    {
+      isOpen && <Overlay className={ classnames(css.ProcessApplicationsOverlay, {
+        'process-application': processApplication,
+        'no-process-application': !processApplication
+      }) } id="process-application-overlay" anchor={ ref.current } onClose={ () => setIsOpen(false) }>
+        {
+          !processApplication
+            ? <>
+              <Section>
+                <Section.Body>
+                  <p>Turn your project into a <a href={ utmTag('https://docs.camunda.io/docs/components/concepts/process-applications/') }>process application</a>:</p>
+                  <ul>
+                    <li>Deploy and test resources (BPMN, DMN, forms) as a single unit</li>
+                    <li>Benefit from cross-file editor intelligence and improved discovery</li>
+                  </ul>
+                  <p>
+                    <button type="button" className="btn btn-primary create-process-application-btn" onClick={ onClickCreateProcessApplication }>
+                      Create a new process application
                     </button>
-                  </li>;
-                })
-              }
-            </ul>
-          </Section.Body>
-        </Section>
+                  </p>
+                </Section.Body>
+              </Section>
+            </>
+            : <>
+              <Section>
+                <Section.Header>
+                  Process application
+                </Section.Header>
+                <Section.Body>
+                  <ul className="files process-application-file" role="menu">
+                    <li role="menuitem" className="file" key={ processApplication.file.path } title={ processApplication.file.path }>
+                      <button type="button" onClick={ () => onRevealInFileExplorer(processApplication.file.path) }>
+                        <ProcessApplicationIcon className="file-icon" width="16" height="16" /><span className="file-name">{ processApplication.file.name }</span>
+                      </button>
+                    </li>
+                  </ul>
+                </Section.Body>
+              </Section>
+              <Section>
+                <Section.Header>
+                  Files
+                </Section.Header>
+                <Section.Body>
+                  <ul className="files process-application-files" role="menu">
+                    {
+                      sortByType(processApplicationItems.filter(item => item.metadata?.type !== 'processApplication')).map(item => {
+                        const { file } = item;
+
+                        const error = hasErrorMessage(item);
+
+                        let Icon;
+
+                        if (error) {
+                          Icon = ErrorIcon;
+                        } else {
+                          const provider = tabsProvider.getProviderForFile(file);
+
+                          Icon = provider?.getIcon(file);
+                        }
+
+                        const isActiveTab = activeTab && activeTab.file.path === file.path;
+
+                        const title = error ? `${ file.path} (${ getErrorMessages(item) })` : file.path;
+
+                        return <li role="menuitem" className={
+                          classnames('file', {
+                            'file-active': isActiveTab,
+                            'file-error': error
+                          })
+                        } key={ file.path } title={ title }>
+                          <button type="button" onClick={ () => onClickOpenPath(file.path) } disabled={ isActiveTab || error }>
+                            {Icon && <Icon className="file-icon" width="16" height="16" />}
+                            <span className="file-name">{ file.name }</span>
+                          </button>
+                        </li>;
+                      })
+                    }
+                  </ul>
+                </Section.Body>
+              </Section>
+            </>
+        }
       </Overlay>
     }
   </>;
@@ -156,4 +192,19 @@ function hasErrorMessage(item) {
 
 function getErrorMessages(item) {
   return item.file.messages?.filter(({ error }) => error).map(({ message }) => message).join(', ');
+}
+
+function isProcessApplicationAllowed(tab) {
+  if (!tab) {
+    return false;
+  }
+
+  const { type } = tab;
+
+  return [
+    'cloud-bpmn',
+    'cloud-dmn',
+    'cloud-form',
+    'rpa'
+  ].includes(type);
 }
