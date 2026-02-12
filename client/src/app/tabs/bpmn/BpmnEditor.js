ed1fd01f6a743e4e9dd994319faf0fb159c12bd8
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2026-02-12 16:37:01 +0100

    feat: add cut capability to BPMN editor
    Closes #1696

--- client/src/app/tabs/bpmn/BpmnEditor.js
+++ client/src/app/tabs/bpmn/BpmnEditor.js
@@ -452,7 +452,7 @@ export class BpmnEditor extends CachedComponent {
       canvasFocused,
       close: true,
       copy: !!selectionLength,
-      cut: false,
+      cut: !!selectionLength,
       duplicate: canvasFocused && !!selectionLength,
       createElement: canvasFocused,
       defaultCopyCutPaste: !canvasFocused,
