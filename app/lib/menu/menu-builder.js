4bc1a24f5839b74e8525f665156cfbf0199c7c58
Ev Bilske <ev.bilske@camunda.com>
2026-03-05 10:57:58 +0100

    fix(menu): correct Save File As ellipsis

--- app/lib/menu/menu-builder.js
+++ app/lib/menu/menu-builder.js
@@ -249,7 +249,7 @@ class MenuBuilder {
 
   appendSaveAsFile() {
     this.menu.append(new MenuItem({
-      label: 'Save File As..',
+      label: 'Save File As...',
       accelerator: 'CommandOrControl+Shift+S',
       enabled: this.options.state.save,
       click: function() {
