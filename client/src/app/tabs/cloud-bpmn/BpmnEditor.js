b0455987a4445ea3b2bf12fa4b1e3caf25cd5f25
Matt Van Horn <455140+mvanhorn@users.noreply.github.com>
2026-03-23 23:48:22 -0700

    fix: disable copy/cut/paste actions for connection only selections
    Connections can't be copied standalone.
    
    The menu items were enabled but did nothing. Now they only enable
    when at least one shape (non-connection) is selected.
    
    Fixes #5690

--- client/src/app/tabs/cloud-bpmn/BpmnEditor.js
+++ client/src/app/tabs/cloud-bpmn/BpmnEditor.js
@@ -457,6 +457,7 @@ export class BpmnEditor extends CachedComponent {
     const selection = modeler.get('selection');
 
     const selectionLength = selection.get().length;
+    const selectionIncludesShapes = selection.get().some(el => !el.waypoints);
 
     const inputActive = isInputActive();
 
@@ -467,10 +468,10 @@ export class BpmnEditor extends CachedComponent {
       appendElement: canvasFocused,
       canvasFocused,
       close: true,
-      copy: !!selectionLength,
-      copyAsImage: !!selectionLength,
-      cut: !!selectionLength,
-      duplicate: canvasFocused && !!selectionLength,
+      copy: selectionIncludesShapes,
+      copyAsImage: selectionIncludesShapes,
+      cut: selectionIncludesShapes,
+      duplicate: canvasFocused && selectionIncludesShapes,
       createElement: canvasFocused,
       defaultCopyCutPaste: !canvasFocused,
       defaultUndoRedo: !canvasFocused,
